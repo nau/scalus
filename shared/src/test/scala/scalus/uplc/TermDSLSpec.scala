bf72de430cb2a7e3adfa7a1788f16d4803946860
Alexander Nemish <anemish@gmail.com>
2022-08-15 14:31:59 +0200

    TermDSL WIP

--- shared/src/test/scala/scalus/uplc/TermDSLSpec.scala
+++ shared/src/test/scala/scalus/uplc/TermDSLSpec.scala
@@ -6,8 +6,10 @@ import scalus.uplc.TermDSL.{*, given}
 
 class TermDSLSpec extends AnyFunSuite with ScalaCheckPropertyChecks:
   test("constant as Term") {
-    val int: Term = BigInt(2)
+    val int: Term = 2
     assert(int == Term.Const(Constant(DefaultUni.Integer, 2)))
+    val bigint: Term = BigInt(2)
+    assert(bigint == Term.Const(Constant(DefaultUni.Integer, 2)))
     val bool: Term = true
     assert(bool == Term.Const(Constant(DefaultUni.Bool, true)))
   }
