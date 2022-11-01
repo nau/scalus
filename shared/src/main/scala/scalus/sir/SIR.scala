fa0d47c7f8d0877930c75371eae916ebf3227f28
Alexander Nemish <anemish@gmail.com>
2022-11-02 00:42:25 +0100

    Pretty SIR colors

--- shared/src/main/scala/scalus/sir/SIR.scala
+++ shared/src/main/scala/scalus/sir/SIR.scala
@@ -3,7 +3,7 @@ package scalus.sir
 import io.bullet.borer.Tag.{NegativeBigNum, Other, PositiveBigNum}
 import io.bullet.borer.encodings.BaseEncoding
 import io.bullet.borer.{Decoder, Encoder, Reader, Writer, DataItem as DI}
-import org.typelevel.paiges.Doc
+import org.typelevel.paiges.{Doc, Style}
 import scalus.uplc.Data.*
 import scalus.uplc.{Constant, Data, DefaultFun, NamedDeBruijn}
 import scalus.utils.Utils
@@ -29,43 +29,42 @@ enum SIR:
   case Builtin(bn: DefaultFun) extends SIR
   case Error(msg: String) extends SIR
 
-  def pretty: Doc = this match
-    case Var(name) => Doc.text(name.name)
-    case Let(Recursivity.NonRec, immutable.List(Binding(name, body)), inExpr) =>
-      body.pretty.bracketBy(
-        Doc.text("let") & Doc.text(name) & Doc.text("="),
-        Doc.text("in")
-      ) + Doc.line + inExpr.pretty
-    case Let(Recursivity.Rec, immutable.List(Binding(name, body)), inExpr) =>
-      val (args, body1) = TermDSL.lamAbsToList(body)
-      val prettyArgs = Doc.intercalate(Doc.text(",") + Doc.line, args.map(Doc.text))
-      prettyArgs.tightBracketBy(
-        Doc.text("fun") & Doc.text(name) + Doc.char('('),
-        Doc
-          .char(')') & Doc.char('=')
-      ) / body1.pretty.indent(2) / Doc.text(
-        "in"
-      ) & inExpr.pretty
-    case LamAbs(name, term) =>
-      val (args, body1) = TermDSL.lamAbsToList(this)
-      val prettyArgs = Doc.stack(args.map(Doc.text))
-      val decl = (Doc.text("{λ") + (Doc.line + prettyArgs & Doc.text("->")).nested(2)).grouped
-      ((decl + (Doc.line + body1.pretty).nested(2)).grouped / Doc.text("}")).grouped.aligned
-    case a @ Apply(f, arg) =>
-      val (t, args) = TermDSL.applyToList(a)
-      val prettyArgs = args match
-        case immutable.List() => Doc.text("()")
-        case _ =>
-          Doc.text("(") + Doc.intercalate(Doc.text(", "), args.map(_.pretty)) + Doc.text(")")
+  def pretty: Doc =
+    def kw(s: String): Doc = Doc.text(s).style(Style.XTerm.Fg.colorCode(172))
+    this match
+      case Var(name) => Doc.text(name.name)
+      case Let(Recursivity.NonRec, immutable.List(Binding(name, body)), inExpr) =>
+        body.pretty.bracketBy(
+          kw("let") & Doc.text(name) & Doc.text("="),
+          kw("in")
+        ) + Doc.line + inExpr.pretty
+      case Let(Recursivity.Rec, immutable.List(Binding(name, body)), inExpr) =>
+        val (args, body1) = TermDSL.lamAbsToList(body)
+        val prettyArgs = Doc.intercalate(Doc.text(",") + Doc.line, args.map(Doc.text))
+        prettyArgs.tightBracketBy(
+          kw("fun") & Doc.text(name) + Doc.char('('),
+          Doc.char(')') & Doc.char('=')
+        ) / body1.pretty.indent(2) / kw("in") & inExpr.pretty
+      case LamAbs(name, term) =>
+        val (args, body1) = TermDSL.lamAbsToList(this)
+        val prettyArgs = Doc.stack(args.map(Doc.text))
+        val decl = (Doc.text("{λ") + (Doc.line + prettyArgs & Doc.text("->")).nested(2)).grouped
+        ((decl + (Doc.line + body1.pretty).nested(2)).grouped / Doc.text("}")).grouped.aligned
+      case a @ Apply(f, arg) =>
+        val (t, args) = TermDSL.applyToList(a)
+        val prettyArgs = args match
+          case immutable.List() => Doc.text("()")
+          case _ =>
+            Doc.text("(") + Doc.intercalate(Doc.text(", "), args.map(_.pretty)) + Doc.text(")")
 
-      t.pretty + prettyArgs
-    case Const(const) => const.prettyValue
-    case IfThenElse(cond, t, f) =>
-      ((Doc.text("if") + (Doc.line + cond.pretty).nested(2)).grouped
-        + (Doc.line + Doc.text("then") + (Doc.line + t.pretty).nested(2)).grouped
-        + (Doc.line + Doc.text("else") + (Doc.line + f.pretty).nested(2)).grouped).aligned
-    case Builtin(bn) => bn.pretty
-    case Error(_)    => Doc.text("ERROR")
+        t.pretty + prettyArgs
+      case Const(const) => const.prettyValue.style(Style.XTerm.Fg.colorCode(64))
+      case IfThenElse(cond, t, f) =>
+        ((kw("if") + (Doc.line + cond.pretty).nested(2)).grouped
+          + (Doc.line + kw("then") + (Doc.line + t.pretty).nested(2)).grouped
+          + (Doc.line + kw("else") + (Doc.line + f.pretty).nested(2)).grouped).aligned
+      case Builtin(bn) => bn.pretty.style(Style.XTerm.Fg.colorCode(176))
+      case Error(_)    => Doc.text("ERROR").style(Style.XTerm.Fg.colorCode(124))
 
 object TermDSL:
   def applyToList(app: SIR): (SIR, immutable.List[SIR]) =
