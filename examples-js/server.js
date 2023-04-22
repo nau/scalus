f264bdcd646dc022b5f6372933f4275466a7d0ed
Alexander Nemish <anemish@gmail.com>
2023-04-22 13:46:59 +0200

    examples files

---
+++ examples-js/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const requestListener = function (req, res) {
+  console.log(req.url)
+  if (req.url === "/target/scala-3.2.1/examples-js-fastopt.js") {
+    fs.readFile(__dirname + "/target/scala-3.2.1/examples-js-fastopt.js")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/javascript");
+            res.writeHead(200);
+            res.end(contents);
+        })
+  } else {
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+  }
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
