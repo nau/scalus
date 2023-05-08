ad41e7bc188308a23c7dc627258e84eb5badea43
Alexander Nemish <anemish@gmail.com>
2023-05-08 18:01:24 +0200

    Update example-js

--- examples-js/server.js
+++ examples-js/server.js
@@ -3,10 +3,17 @@ const fs = require('fs').promises;
 
 const requestListener = function (req, res) {
   console.log(req.url)
-  if (req.url === "/target/scala-3.2.2/examples-js-fastopt.js") {
+  if (req.url === "/examples-js-fastopt.js") {
     fs.readFile(__dirname + "/target/scala-3.2.2/examples-js-fastopt.js")
         .then(contents => {
             res.setHeader("Content-Type", "text/javascript");
+            res.writeHead(200);
+            res.end(contents);
+        })
+  } else if (req.url === "/apikey") {
+    fs.readFile(__dirname + "/../blockfrost_api_key.txt")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/plain");
             res.writeHead(200);
             res.end(contents);
         })
