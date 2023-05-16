4e5517d5b996d994afe437a8122d2aebbd82ed43
Alexander Nemish <anemish@gmail.com>
2023-05-16 16:48:11 +0200

    Eta reduce for SIR and Term, tests fail

--- shared/src/main/scala/scalus/sir/SimpleSirToUplcLowering.scala
+++ shared/src/main/scala/scalus/sir/SimpleSirToUplcLowering.scala
@@ -27,7 +27,7 @@ class SimpleSirToUplcLowering(generateErrorTraces: Boolean = false) {
   private val decls = HashMap.empty[String, DataDecl]
 
   def lower(sir: SIR): Term =
-    val term = lowerInner(sir)
+    val term = etaReduce(lowerInner(sir))
     if zCombinatorNeeded then Term.Apply(Term.LamAbs("__z_combinator__", term), ExprBuilder.ZTerm)
     else term
 
@@ -123,11 +123,11 @@ class SimpleSirToUplcLowering(generateErrorTraces: Boolean = false) {
   def etaReduce(term: Term): Term =
     import Term.*
     term match
-      // FIXME: check f for free occurrences of name1, and check for ERROR in f
-      case LamAbs(name1, Term.Apply(f, Term.Var(name2))) if name1 == name2.name =>
-        /* println(
+      case LamAbs(name1, Term.Apply(f, Term.Var(name2)))
+          if name1 == name2.name && !freeNames(f, List.empty).contains(name1) && notError(f) =>
+        println(
           s"etaReducing ${term.pretty.render(80).take(50)} to ${f.pretty.render(80).take(50)}"
-        ) */
+        )
         etaReduce(f)
       case LamAbs(name, body) =>
         val body1 = etaReduce(body)
@@ -136,4 +136,125 @@ class SimpleSirToUplcLowering(generateErrorTraces: Boolean = false) {
       case Force(term)   => Force(etaReduce(term))
       case Delay(term)   => Delay(etaReduce(term))
       case _             => term
+
+  def freeNames(term: Term, env: List[String]): Set[String] =
+    import Term.*
+    term match
+      case Var(NamedDeBruijn(name, _)) => if env.contains(name) then Set.empty else Set(name)
+      case LamAbs(name, body)          => freeNames(body, name :: env)
+      case Apply(f, arg)               => freeNames(f, env) ++ freeNames(arg, env)
+      case Force(term)                 => freeNames(term, env)
+      case Delay(term)                 => freeNames(term, env)
+      case Const(_)                    => Set.empty
+      case Error(_)                    => Set.empty
+      case Builtin(bn)                 => Set.empty
+
+  def freeNames(term: SIR, env: List[String]): Set[String] =
+    import SIR.*
+    term match
+      case Var(name) =>
+        if env.contains(name) then Set.empty else Set(name)
+      case ExternalVar(_, name) =>
+        if env.contains(name) then Set.empty else Set(name)
+      case LamAbs(name, body) =>
+        freeNames(body, name :: env)
+      case Apply(f, arg) =>
+        freeNames(f, env) ++ freeNames(arg, env)
+      case Let(_, bindings, body) =>
+        val env1 = bindings.map(_.name) ++ env
+        bindings.foldLeft(freeNames(body, env1)) { case (acc, Binding(_, rhs)) =>
+          acc ++ freeNames(rhs, env1)
+        }
+      case Match(scrutinee, cases) =>
+        freeNames(scrutinee, env) ++ cases.foldLeft(Set.empty[String]) {
+          case (acc, Case(_, bindings, body)) =>
+            acc ++ freeNames(body, bindings ++ env)
+        }
+      case Const(_) => Set.empty
+      case And(lhs, rhs) =>
+        freeNames(lhs, env) ++ freeNames(rhs, env)
+      case Or(lhs, rhs) =>
+        freeNames(lhs, env) ++ freeNames(rhs, env)
+      case Not(term) =>
+        freeNames(term, env)
+      case IfThenElse(cond, t, f) =>
+        freeNames(cond, env) ++ freeNames(t, env) ++ freeNames(f, env)
+      case Builtin(_)       => Set.empty
+      case Error(_)         => Set.empty
+      case Decl(data, term) => freeNames(term, env)
+      case Constr(name, data, args) =>
+        args.foldLeft(Set.empty[String]) { case (acc, arg) =>
+          acc ++ freeNames(arg, env)
+        }
+
+  def notError(term: SIR): Boolean =
+    import SIR.*
+    term match
+      case Error(_)                => false
+      case Decl(data, term)        => notError(term)
+      case Constr(_, _, args)      => args.forall(notError)
+      case Apply(f, a)             => notError(f) && notError(a)
+      case LamAbs(_, body)         => notError(body)
+      case Let(_, bindings, body)  => bindings.forall(b => notError(b.value)) && notError(body)
+      case Match(scrutinee, cases) => notError(scrutinee)
+      case Const(_)                => true
+      case And(lhs, rhs)           => notError(lhs) && notError(rhs)
+      case Or(lhs, rhs)            => notError(lhs) && notError(rhs)
+      case Not(term)               => notError(term)
+      case IfThenElse(c, t, f)     => notError(c) && notError(t) && notError(f)
+      case Builtin(_)              => true
+      case ExternalVar(_, _)       => true
+      case Var(_)                  => true
+
+  def notError(term: Term): Boolean =
+    import Term.*
+    term match
+      case Error(_)        => false
+      case Apply(f, a)     => notError(f) && notError(a)
+      case LamAbs(_, body) => notError(body)
+      case Force(term)     => notError(term)
+      case Delay(term)     => notError(term)
+      case Const(_)        => true
+      case Builtin(_)      => true
+      case Var(_)          => true
+
+  def etaReduceSIR(term: SIR): SIR =
+    import SIR.*
+    import scalus.pretty
+    // println(s"eta: ${term.pretty.render(80).take(50)}")
+    // println(s"eta: ${term.toString.take(100)}")
+    term match
+      // \x. f x -> f
+      // \x -> \ y -> ((f x) y) => f
+      case LamAbs(name1, Apply(f, Var(name2)))
+          if name1 == name2 && !freeNames(f, List.empty).contains(name1) && notError(f) =>
+        println(
+          s"etaReducing ${term.pretty.render(80).take(50)} to ${f.pretty.render(80).take(50)}"
+        )
+        etaReduceSIR(f)
+      case LamAbs(name, body) =>
+        // println(s"lam $name: ${term.toString.take(100)}")
+        val body1 = etaReduceSIR(body)
+        if body != body1 then etaReduceSIR(LamAbs(name, body1)) else term
+      case Apply(f, arg) => Apply(etaReduceSIR(f), etaReduceSIR(arg))
+      case Let(recursivity, bindings, body) =>
+        Let(
+          recursivity,
+          bindings.map(b => b.copy(value = etaReduceSIR(b.value))),
+          etaReduceSIR(body)
+        )
+      case IfThenElse(cond, t, f) => IfThenElse(cond, etaReduceSIR(t), etaReduceSIR(f))
+      case Match(scrutinee, cases) =>
+        Match(
+          scrutinee,
+          cases.map { case Case(constr, bindings, body) =>
+            Case(constr, bindings, etaReduceSIR(body))
+          }
+        )
+      case Constr(name, data, args) => Constr(name, data, args.map(etaReduceSIR))
+      case And(a, b)                => And(etaReduceSIR(a), etaReduceSIR(b))
+      case Or(a, b)                 => Or(etaReduceSIR(a), etaReduceSIR(b))
+      case Not(a)                   => Not(etaReduceSIR(a))
+      case Decl(data, term)         => Decl(data, etaReduceSIR(term))
+      case _                        => term
 }
