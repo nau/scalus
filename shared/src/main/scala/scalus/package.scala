2cc015278f564ab333f62365f09e858c85816216
Alexander Nemish <anemish@gmail.com>
2023-05-28 20:18:24 +0200

    Test deserialization with Scalus scripts

--- shared/src/main/scala/scalus/package.scala
+++ shared/src/main/scala/scalus/package.scala
@@ -3,8 +3,20 @@ import scalus.sir.PrettyPrinter
 import scalus.uplc.DefaultUni
 import org.typelevel.paiges.Doc
 import scalus.uplc.Constant
+import scalus.sir.SimpleSirToUplcLowering
+import scalus.uplc.ProgramFlatCodec
+import scalus.uplc.Program
+import scalus.utils.Hex
 package object scalus {
-  extension (sir: SIR) def pretty: Doc = PrettyPrinter.pretty(sir)
+  extension (sir: SIR) 
+    def pretty: Doc = PrettyPrinter.pretty(sir)
+    def doubleCborHex(version: (Int, Int, Int), generateErrorTraces: Boolean = false): String = 
+      import io.bullet.borer.Cbor
+      val term = new SimpleSirToUplcLowering(generateErrorTraces).lower(sir)
+      val flatEncoded = ProgramFlatCodec.encodeFlat(Program(version, term))
+      val cbor = Cbor.encode(flatEncoded).toByteArray
+      val cbor2 = Cbor.encode(cbor).toByteArray
+      Hex.bytesToHex(cbor2)
   extension (du: DefaultUni) def pretty: Doc = PrettyPrinter.pretty(du)
   extension (c: Constant) def pretty: Doc = PrettyPrinter.pretty(c)
 }
