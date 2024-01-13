5dbd6de41fdcf0297fe28918dc22eebc64c72e45
Alexander Nemish <anemish@gmail.com>
2024-01-13 17:13:40 +0100

    build: update flakes

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   inputs = {
-    nixpkgs.follows = "haskellNix/nixpkgs";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    # nixpkgs.follows = "haskellNix/nixpkgs";
+    # haskellNix.url = "github:input-output-hk/haskell.nix";
+    # iohk-nix.url = "github:input-output-hk/iohk-nix";
     flake-utils.url = "github:numtide/flake-utils";
-    CHaP = {
-      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
-      flake = false;
-    };
-    cardano-node.url = "github:input-output-hk/cardano-node/1.35.7";
-    plutus.url = "github:input-output-hk/plutus/914b7f3108362cfa925810af8082d2ad5564c7b2";
+    # CHaP = {
+      # url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      # flake = false;
+    # };
+    # cardano-node.url = "github:input-output-hk/cardano-node/1.35.7";
+    plutus.url = "github:input-output-hk/plutus/e2cbee0d31da1b2dfa42cc76fb112dc69fa06798";
     /* gitignore-nix = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,7 +20,7 @@
     { self
     , flake-utils
     , nixpkgs
-    , cardano-node
+    # , cardano-node
     , plutus
     , ...
     } @ inputs:
@@ -29,10 +29,10 @@
       let
         pkgs = import nixpkgs { inherit system; };
         uplc = plutus.${system}.plutus.library.plutus-project.hsPkgs.plutus-core.components.exes.uplc;
-        patchedUplc = uplc.overrideAttrs (oldAttrs: {
-          patches = oldAttrs.patches or [] ++ [ ./uplc.patch ];
-          patchFlags = [ "-p2" ];
-        });
+        # patchedUplc = uplc.overrideAttrs (oldAttrs: {
+          # patches = oldAttrs.patches or [] ++ [ ./uplc.patch ];
+          # patchFlags = [ "-p2" ];
+        # });
       in
       rec {
         devShell = pkgs.mkShell {
@@ -51,9 +51,9 @@
             uplc
             # patchedUplc
           ];
-          shellHook = ''
-            ln -s ${plutus}/plutus-conformance plutus-conformance
-          '';
+          # shellHook = ''
+          #   ln -s ${plutus}/plutus-conformance plutus-conformance
+          # '';
         };
       })
     );
@@ -61,13 +61,9 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.iog.io"
-      "https://hydra-node.cachix.org"
-      "https://cache.zw3rk.com"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "hydra-node.cachix.org-1:vK4mOEQDQKl9FTbq76NjOuNaRD4pZLxi1yri31HHmIw="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
     allow-import-from-derivation = true;
   };
