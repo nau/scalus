886c8d0981ddc0fa2cd53dd3df6e3de38d267465
Alexander Nemish <anemish@gmail.com>
2024-05-21 15:44:06 +0200

    test: MemoryUsageSpec

---
+++ shared/src/test/scala/scalus/uplc/eval/MemoryUsageSpec.scala
@@ -0,0 +1,34 @@
+package scalus.uplc.eval
+
+import org.scalacheck.Gen
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import scalus.*
+import scalus.uplc.ArbitraryInstances
+import scalus.uplc.DefaultUni.asConstant
+import scalus.uplc.Term.*
+import scalus.uplc.eval.VM
+
+class MemoryUsageSpec extends AnyFunSuite with ScalaCheckPropertyChecks with ArbitraryInstances {
+    test("memoryUsageInteger(0) == 1") {
+        assert(MemoryUsage.memoryUsageInteger(0) == 1)
+        assert(MemoryUsage.memoryUsageInteger2(0) == 1)
+    }
+
+    test("memoryUsageInteger(2^64) == 1") {
+        assert(MemoryUsage.memoryUsageInteger(BigInt(2).pow(64) - 1) == 1)
+        assert(MemoryUsage.memoryUsageInteger2(BigInt(2).pow(64) - 1) == 1)
+        assert(MemoryUsage.memoryUsageInteger(BigInt(2).pow(64)) == 2)
+        assert(MemoryUsage.memoryUsageInteger2(BigInt(2).pow(64)) == 2)
+        assert(MemoryUsage.memoryUsageInteger(BigInt(2).pow(64) + 1) == 2)
+        assert(MemoryUsage.memoryUsageInteger2(BigInt(2).pow(64) + 1) == 2)
+    }
+
+    test("MemoryUsage.memoryUsageInteger(x) == MemoryUsage.memoryUsageInteger2(x)") {
+        forAll { (x: BigInt) =>
+            println(x)
+            assert(MemoryUsage.memoryUsageInteger(x) == MemoryUsage.memoryUsageInteger2(x))
+            assert(MemoryUsage.memoryUsageInteger(-x) == MemoryUsage.memoryUsageInteger2(-x))
+        }
+    }
+}
