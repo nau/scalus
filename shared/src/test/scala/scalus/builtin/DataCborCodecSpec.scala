a0071d04fa07a6c2fd5bb5f2e07eeac4381807d3
Alexander Nemish <anemish@gmail.com>
2024-05-23 11:19:13 +0200

    fix: Data CBOR encoding/decoding
    On Cardano, Plutus Data ByteStrings and BigInts that are represented as ByteString must be serialized chunked up to 64 bytes.

--- shared/src/test/scala/scalus/builtin/DataCborCodecSpec.scala
+++ shared/src/test/scala/scalus/builtin/DataCborCodecSpec.scala
@@ -13,86 +13,95 @@ import scala.collection.immutable
 
 class DataCborCodecSpec extends AnyFunSuite with ScalaCheckPropertyChecks with ArbitraryInstances:
 
-    implicit val plutusDataCborEncoder: Encoder[Data] = PlutusDataCborEncoder
-    implicit val plutusDataCborDecoder: Decoder[Data] = PlutusDataCborDecoder
+    given Encoder[Data] = PlutusDataCborEncoder
+    given Decoder[Data] = PlutusDataCborDecoder
 
-    def encodeAsHexString(d: Data) = Utils.bytesToHex(Cbor.encode(d).toByteArray)
-    def roundtrip(d: Data): Unit =
+    private def encodeHex(d: Data) = Utils.bytesToHex(Cbor.encode(d).toByteArray)
+    private def decodeHex(d: String) = Cbor.decode(Utils.hexToBytes(d)).to[Data].value
+
+    private def roundtrip(d: Data): Unit =
         val ba = Cbor.encode(d).toByteArray
-//    println(s"$d => ${ba.map("%02X" format _).mkString(" ")}")
         val dd = Cbor.decode(ba).to[Data].value
-        //      println(s"$dd")
         assert(d == dd)
 
-    test("Encoder <-> Decoder") {
-        forAll { (d: Data) =>
-            roundtrip(d)
+    test("BigInt encoding/decoding of < 64 bits") {
+        val bigInt = BigInt("1234567890111213141516")
+        val expected = "c24942ed123b08fe58fe0c"
+        assert(encodeHex(I(bigInt)) == expected)
+        assert(decodeHex(expected) == I(bigInt))
+    }
+
+    test("BigInt encoding/decoding of > 64 bits is chunked") {
+        val twoTo520 = BigInt(2).pow(520)
+        val expectedTwo =
+            "c25f584001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000420000ff"
+        assert(encodeHex(I(twoTo520)) == expectedTwo)
+        assert(decodeHex(expectedTwo) == I(twoTo520))
+
+        val minusTwoTo520 = -BigInt(2).pow(520)
+        val expectedMinusTwo =
+            "c35f584000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff42ffffff"
+        assert(encodeHex(I(minusTwoTo520)) == expectedMinusTwo)
+        assert(decodeHex(expectedMinusTwo) == I(minusTwoTo520))
+    }
+
+    test("All BigInt values are encoded/decoded correctly") {
+        forAll { (bigInt: BigInt) =>
+            roundtrip(I(bigInt))
         }
     }
 
-    test("PlutusDataCborEncoder") {
+    test("Encoding/decoding of Constrs") {
+        assert(encodeHex(Constr(3, Constr(3, Nil) :: Nil)) == "d87c9fd87c80ff")
+        assert(decodeHex("d8 7c 9f d8 7c 80 ff") == Constr(3, Constr(3, Nil) :: Nil))
 
-        // byte array to hex string
+        assert(encodeHex(List(Constr(3, Nil) :: Nil)) == "9fd87c80ff")
+        assert(decodeHex("9f d8 7c 80 ff") == List(Constr(3, Nil) :: Nil))
 
-        assert(
-          encodeAsHexString(Constr(3, Constr(3, Nil) :: Nil)) == "d87c9fd87c80ff"
-        )
+        assert(encodeHex(List(Constr(7, Nil) :: Nil)) == "9fd9050080ff")
+        assert(decodeHex("9f d9 05 00 80 ff") == List(Constr(7, Nil) :: Nil))
 
+        assert(encodeHex(List(Constr(1234567890, Nil) :: Nil)) == "9fd866821a499602d280ff")
         assert(
-          Cbor.decode(Utils.hexToBytes("d8 7c 9f d8 7c 80 ff")).to[Data].value == Constr(
-            3,
-            Constr(3, Nil) :: Nil
-          )
+          decodeHex("9f d8 66 82 1a 49 96 02 d2 80 ff") == List(Constr(1234567890, Nil) :: Nil)
         )
+    }
 
+    test("Encoding/decoding of Lists") {
         assert(
-          encodeAsHexString(List(Constr(3, Nil) :: Nil)) == "9fd87c80ff"
-        )
-        assert(
-          encodeAsHexString(List(Constr(7, Nil) :: Nil)) == "9fd9050080ff"
-        )
-        assert(
-          encodeAsHexString(
-            List(Constr(1234567890, Nil) :: Nil)
-          ) == "9fd866821a499602d280ff"
-        )
-        assert(
-          encodeAsHexString(
+          encodeHex(
             List(
               I(0) :: I(-1) :: I(100) :: I(1000) :: I(BigInt("1234567890111213141516")) :: Nil
             ): Data
           ) == "9f002018641903e8c24942ed123b08fe58fe0cff"
         )
         assert(
-          encodeAsHexString(B(builtin.ByteString.unsafeFromArray("12".getBytes))) == "423132"
+          decodeHex("9f 00 20 18 64 19 03 e8 c2 49 42 ed 12 3b 08 fe 58 fe 0c ff") ==
+              (List(
+                I(0) :: I(-1) :: I(100) :: I(1000) :: I(BigInt("1234567890111213141516")) :: Nil
+              ): Data)
         )
     }
 
-    test("ByteString < 64") {
+    test("ByteString <= 64") {
         val longBs = "1234567890"
-        val expected =
-            "4a31323334353637383930"
-        assert(
-          encodeAsHexString(B(builtin.ByteString.unsafeFromArray(longBs.getBytes))) == expected
-        )
-        assert(
-          Cbor.decode(Utils.hexToBytes(expected)).to[Data].value == B(
-            builtin.ByteString.unsafeFromArray(longBs.getBytes)
-          )
-        )
+        val expected = "4a31323334353637383930"
+        assert(encodeHex(B(builtin.ByteString.unsafeFromArray(longBs.getBytes))) == expected)
+        assert(decodeHex(expected) == B(builtin.ByteString.unsafeFromArray(longBs.getBytes)))
     }
+
     test("ByteString chunks") {
         val longBs = "1234567890" * 7
         val expected =
             "5f58403132333435363738393031323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333446353637383930ff"
-        assert(
-          encodeAsHexString(B(builtin.ByteString.unsafeFromArray(longBs.getBytes))) == expected
-        )
-        assert(
-          Cbor.decode(Utils.hexToBytes(expected)).to[Data].value == B(
-            builtin.ByteString.unsafeFromArray(longBs.getBytes)
-          )
-        )
+        assert(encodeHex(B(builtin.ByteString.fromArray(longBs.getBytes))) == expected)
+        assert(decodeHex(expected) == B(builtin.ByteString.fromArray(longBs.getBytes)))
+    }
+
+    test("All ByteString values are encoded/decoded correctly") {
+        forAll { (bs: ByteString) =>
+            roundtrip(B(bs))
+        }
     }
 
     test("Map can have duplicate keys") {
@@ -121,4 +130,10 @@ class DataCborCodecSpec extends AnyFunSuite with ScalaCheckPropertyChecks with A
               )
             )
         roundtrip(data)
+    }
+
+    test("Encoder <-> Decoder roundtrip") {
+        forAll { (d: Data) =>
+            roundtrip(d)
+        }
     }
