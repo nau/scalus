4593452553ae3832ffe736e7bdb42b794c4919a5
Alexander Nemish <anemish@gmail.com>
2024-08-14 14:29:48 +0200

    docs: add docstrings to Program

--- shared/src/main/scala/scalus/uplc/Program.scala
+++ shared/src/main/scala/scalus/uplc/Program.scala
@@ -8,6 +8,16 @@ import scalus.utils.Hex
 
 import scala.annotation.targetName
 
+/** A Cardano Plutus program.
+  *
+  * A program is a versioned [[Term]]. The version is a tuple of three integers: major, minor, and
+  * patch.
+  *
+  * @param version
+  *   the version of the program
+  * @param term
+  *   the term of the program
+  */
 case class Program(version: (Int, Int, Int), term: Term):
     /** Checks if two programs are equal.
       *
@@ -16,28 +26,84 @@ case class Program(version: (Int, Int, Int), term: Term):
       * We use unique negative indices to represent free variables.
       *
       * @param that
+      *   the other program
       * @return
+      *   true if the programs are alpha-equal, false otherwise
       */
     infix def alphaEq(that: Program): Boolean =
         version == that.version && Term.alphaEq(this.term, that.term)
-    lazy val flatEncoded = ProgramFlatCodec.encodeFlat(this)
-    lazy val cborEncoded = Cbor.encode(flatEncoded).toByteArray
-    lazy val doubleCborEncoded = Cbor.encode(cborEncoded).toByteArray
-    lazy val doubleCborHex = Hex.bytesToHex(doubleCborEncoded)
+
+    /** Flat-encoded representation of the program.
+      *
+      * The flat-encoded representation is a byte array that contains the program in a flat format.
+      * This format is used to serialize the program to CBOR.
+      */
+    lazy val flatEncoded: Array[Byte] = ProgramFlatCodec.encodeFlat(this)
+
+    /** CBOR-encoded representation of the program.
+      *
+      * The CBOR-encoded representation is a byte array that contains the program in a CBOR format.
+      */
+    lazy val cborEncoded: Array[Byte] = Cbor.encode(flatEncoded).toByteArray
+
+    /** Double CBOR-encoded representation of the program.
+      *
+      * Cardano uses a double-CBOR encoding for Plutus scripts in many places.
+      */
+    lazy val doubleCborEncoded: Array[Byte] = Cbor.encode(cborEncoded).toByteArray
+
+    /** Double CBOR-encoded hex string of the program.
+      *
+      * Cardano uses a double-CBOR encoding for Plutus scripts in many places.
+      */
+    lazy val doubleCborHex: String = Hex.bytesToHex(doubleCborEncoded)
+
+    /** Applies an argument to the program.
+      *
+      * @param arg
+      *   the argument
+      * @return
+      *   the program with the argument applied
+      */
     @targetName("applyArg")
     infix def $(arg: Term): Program = Program(version, Term.Apply(term, arg))
 
 object Program:
+    /** Deserializes a program from a double-CBOR-encoded hex string.
+      *
+      * @param doubleCborHex
+      *   the double-CBOR-encoded hex string
+      * @return
+      *   the program
+      */
     def fromDoubleCborHex(doubleCborHex: String): Program =
         val cbor = Cbor.decode(Hex.hexToBytes(doubleCborHex)).to[Array[Byte]].value
         val scriptFlat = Cbor.decode(cbor).to[Array[Byte]].value
         val debruijnedProgram = ProgramFlatCodec.decodeFlat(scriptFlat)
         debruijnedProgram.toProgram
+
+    /** Deserializes a program from a CBOR-encoded hex string.
+      *
+      * @param cborHex
+      *   the CBOR-encoded hex string
+      * @return
+      *   the program
+      */
     def fromCborHex(cborHex: String): Program =
         val cbor = Cbor.decode(Hex.hexToBytes(cborHex)).to[Array[Byte]].value
         val debruijnedProgram = ProgramFlatCodec.decodeFlat(cbor)
         debruijnedProgram.toProgram
 
+/** A De Bruijn-indexed program.
+  *
+  * A De Bruijn-indexed program is a versioned [[Term]] where the variables are indexed using De
+  * Bruijn indices. A program must be De Bruijn-indexed before it can be evaluated.
+  *
+  * @param version
+  *   the version of the program
+  * @param term
+  *   the term of the program
+  */
 case class DeBruijnedProgram private[uplc] (version: (Int, Int, Int), term: Term):
     def pretty: Doc =
         val (major, minor, patch) = version
