2b1a13ced2fea8590bc2176d1f600670a0597ba7
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2024-11-07 11:55:08 +0200

    updated tesat for SIRCompiler

--- scalus-plugin/src/test/scala/scalus/flat/SIRFlatSerializationSpec.scala
+++ scalus-plugin/src/test/scala/scalus/flat/SIRFlatSerializationSpec.scala
@@ -7,18 +7,20 @@ import scalus.flat.DecoderState
 import scalus.flat.EncoderState
 import scalus.flat.Flat
 import scalus.flat.FlatInstantces.given
-import scalus.sir.SIR
+import scalus.sir.{Binding, Module, SIR, SIRType}
 
 class SIRFlatSerializationSpec extends AnyFunSuite with ScalaCheckPropertyChecks:
 
-    test("serialize and deserialize SIR") {
-        val fl = summon[Flat[SIR]]
-        val sir = SIR.Const(scalus.uplc.Constant.Integer(23))
-        val enc = EncoderState(fl.bitSize(sir) / 8 + 1)
-        flat.encode(sir, enc)
+    test("serialize and deserialize SIR Module") {
+        val fl = summon[Flat[Module]]
+        val sir = SIR.Const(scalus.uplc.Constant.Integer(23), SIRType.IntegerPrimitive)
+        val binding = Binding("x", sir)
+        val module = Module((1, 0), List(binding))
+        val enc = EncoderState(fl.bitSize(module) / 8 + 1)
+        flat.encode(module, enc)
         enc.filler()
         val dec = DecoderState(enc.buffer)
-        val sir2 = flat.decode[SIR](dec)
-        assert(sir == sir2)
+        val module2 = flat.decode[Module](dec)
+        assert(module == module2)
     }
 
