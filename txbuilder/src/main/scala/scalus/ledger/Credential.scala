1471342a19f42c6a266d47f1a125affb0af883a4
Alexander Nemish <anemish@gmail.com>
2025-03-26 19:47:52 +0100

    derives Codec

--- txbuilder/src/main/scala/scalus/ledger/Credential.scala
+++ txbuilder/src/main/scala/scalus/ledger/Credential.scala
@@ -1,16 +1,19 @@
 package scalus.ledger
 
-import io.bullet.borer.{Decoder, Encoder, Reader, Writer}
-import scalus.builtin.ByteString
+import io.bullet.borer.Codec
+import io.bullet.borer.derivation.ArrayBasedCodecs.*
+import io.bullet.borer.derivation.key
 
 /** Represents a credential in the Cardano blockchain. A credential can be either a key hash or a
   * script hash.
   */
-enum Credential:
+enum Credential derives Codec.All:
     /** Key hash credential */
+    @key(0)
     case KeyHash(hash: Hash28)
 
     /** Script hash credential */
+    @key(1)
     case ScriptHash(hash: Hash28)
 
     /** Check if this credential is a key hash */
@@ -22,33 +25,3 @@ enum Credential:
     def isScriptHash: Boolean = this match
         case ScriptHash(_) => true
         case _             => false
-
-object Credential:
-    /** CBOR encoder for Credential */
-    given Encoder[Credential] with
-        def write(w: Writer, value: Credential): Writer = value match
-            case Credential.KeyHash(hash) =>
-                w.writeArrayHeader(2)
-                    .writeInt(0)
-                    .writeBytes(hash.bytes.bytes)
-
-            case Credential.ScriptHash(hash) =>
-                w.writeArrayHeader(2)
-                    .writeInt(1)
-                    .writeBytes(hash.bytes.bytes)
-
-    /** CBOR decoder for Credential */
-    given Decoder[Credential] with
-        def read(r: Reader): Credential =
-            r.readArrayHeader()
-            val tag = r.readInt()
-
-            tag match
-                case 0 =>
-                    val hash = ByteString.unsafeFromArray(r.readBytes())
-                    Credential.KeyHash(Hash28(hash))
-                case 1 =>
-                    val hash = ByteString.unsafeFromArray(r.readBytes())
-                    Credential.ScriptHash(Hash28(hash))
-                case _ =>
-                    r.validationFailure(s"Invalid credential type: $tag, expected 0 or 1")
