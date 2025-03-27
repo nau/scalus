22efb43549bf6985c4182b2129bf8dabf92329f1
Alexander Nemish <anemish@gmail.com>
2025-03-27 16:48:04 +0100

    txdownloader

--- bloxbean-cardano-client-lib/src/test/scala/scalus/bloxbean/TxDownloader.scala
+++ bloxbean-cardano-client-lib/src/test/scala/scalus/bloxbean/TxDownloader.scala
@@ -5,18 +5,18 @@ import com.bloxbean.cardano.yaci.core.protocol.chainsync.messages.Point
 import com.bloxbean.cardano.yaci.helper.BlockRangeSync
 import com.bloxbean.cardano.yaci.helper.listener.BlockChainDataListener
 import com.bloxbean.cardano.yaci.helper.model.Transaction
+import scalus.utils.Utils
 
-import java.nio.file.Files
-import java.nio.file.Path
+import java.nio.file.{Files, Path, StandardOpenOption}
 import java.util
 
 object TxDownloader {
 
     @main
-    def main(): Unit = {
+    def main(args: String*): Unit = {
         YaciConfig.INSTANCE.setReturnBlockCbor(true)
-        val blockRangeSync =
-            new BlockRangeSync("178.18.251.55", 6000, 764824073L);
+        val host = args.headOption.getOrElse("localhost")
+        val blockRangeSync = new BlockRangeSync(host, 3001, 764824073L);
         blockRangeSync.start(new BlockChainDataListener() {
             override def onBlock(
                 era: Era,
@@ -24,19 +24,27 @@ object TxDownloader {
                 transactions: util.List[Transaction]
             ): Unit = {
                 println(s"Block ${block.getHeader.getHeaderBody.getBlockNumber}")
-                Files.write(
-                  Path.of(
-                    s"/Users/nau/projects/scalus/bloxbean-cardano-client-lib/blocks/block-${block.getHeader.getHeaderBody.getBlockNumber}.cbor"
-                  ),
-                  util.List.of(block.getCbor)
-                )
+                try
+                    val cborBytes = Utils.hexToBytes(block.getCbor)
+                    Files.createDirectories(Path.of("blocks"))
+                    Files.write(
+                      Path.of(
+                        s"blocks/block-${block.getHeader.getHeaderBody.getBlockNumber}.cbor"
+                      ),
+                      cborBytes,
+                      StandardOpenOption.TRUNCATE_EXISTING,
+                      StandardOpenOption.WRITE,
+                      StandardOpenOption.CREATE
+                    )
+                catch
+                    case e: Exception =>
+                        e.printStackTrace()
             }
         })
-        // full epoch 484,
         val from =
-            Point(134092810, "bd14aacfeb71fb52313b58cc0707634eee04ac71e61c438de83ea8ee8b7bd31a")
+            Point(151372807, "7325c3a11f265e60e78ce8fbb08cc109d3cf432d47c8d2d5dd432803d56a9147")
         val to =
-            Point(134524753, "29011cc1320d03b3da0121236dc66e6bc391feef4bb1d506a7fb20e769d6a494")
+            Point(151516583, "7462f5d03a4a4a0e11f77964b66a630184d39e437acb5a3104e66b4aa73907de")
         blockRangeSync.fetch(from, to)
     }
 
