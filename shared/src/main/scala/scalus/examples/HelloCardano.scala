228c811186828a38e63f54573a61c68c82121ef0
Alexander Nemish <anemish@gmail.com>
2025-04-01 11:34:29 +0200

    feat: initial HelloCardano contract and test

---
+++ shared/src/main/scala/scalus/examples/HelloCardano.scala
@@ -0,0 +1,36 @@
+package scalus.examples
+
+import scalus.*
+import scalus.Compiler.compile
+import scalus.builtin.Data
+import scalus.ledger.api.v3.{PubKeyHash, ScriptContext, ScriptInfo}
+import scalus.ledger.api.v1.FromDataInstances.given
+import scalus.ledger.api.v3.FromDataInstances.given
+import scalus.builtin.FromDataInstances.given
+import scalus.prelude.*
+
+@Compile
+object HelloCardano {
+    def validator(scriptContext: Data): Unit = {
+        val ctx = scriptContext.to[ScriptContext]
+        ctx.scriptInfo match
+            case ScriptInfo.SpendingScript(txOutRef, datum) =>
+                datum match
+                    case Maybe.Just(ownerDatum) =>
+                        val owner = ownerDatum.to[PubKeyHash]
+                        // must be signed
+                        List.findOrFail(ctx.txInfo.signatories)(signatory =>
+                            signatory.hash == owner.hash
+                        )
+                        val mustSayHello = ctx.redeemer.to[String] == "Hello, Cardano!"
+                        if !mustSayHello then throw new Exception("Invalid message")
+                    case Maybe.Nothing => throw new Exception("Expected datum")
+            case _ => throw new Exception("Invalid script type")
+    }
+}
+
+object HelloCardanoValidator {
+    val sir = compile(HelloCardano.validator)
+    // UPLC program: (ScriptContext as Data) -> ()
+    val script = sir.toUplc(generateErrorTraces = true).plutusV3
+}
