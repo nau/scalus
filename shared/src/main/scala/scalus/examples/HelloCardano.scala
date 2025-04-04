2637fd01022bd21e1db59ea8706c4760be618316
Alexander Nemish <anemish@gmail.com>
2025-04-04 18:37:34 +0200

    wip: simplify HelloCardano

--- shared/src/main/scala/scalus/examples/HelloCardano.scala
+++ shared/src/main/scala/scalus/examples/HelloCardano.scala
@@ -5,6 +5,7 @@ import scalus.builtin.Data
 import scalus.builtin.FromDataInstances.given
 import scalus.ledger.api.v1.FromDataInstances.given
 import scalus.ledger.api.v3.FromDataInstances.given
+import scalus.ledger.api.v3.ScriptInfo.SpendingScript
 import scalus.ledger.api.v3.{PubKeyHash, ScriptContext, ScriptInfo}
 import scalus.prelude.*
 import scalus.prelude.Prelude.*
@@ -14,12 +15,12 @@ object HelloCardano {
     def validator(scriptContext: Data): Unit = {
         val ctx = scriptContext.to[ScriptContext]
         ctx.scriptInfo match
-            case ScriptInfo.SpendingScript(txOutRef, datum) =>
+            case SpendingScript(_, datum) =>
                 val owner = datum.getOrFail("Expected datum").to[PubKeyHash]
                 val mustBeSigned = ctx.txInfo.signatories.find { _.hash == owner.hash }
                 mustBeSigned orFail "Must be signed"
                 val mustSayHello = ctx.redeemer.to[String] == "Hello, Cardano!"
                 require(mustSayHello, "Invalid redeemer")
-            case _ => throw new Exception("Invalid script type")
+            case _ => fail("Must be spending")
     }
 }
