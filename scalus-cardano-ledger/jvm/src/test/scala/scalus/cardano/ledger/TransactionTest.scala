30b6668580b0eb7dbca1d94926014b7e2a9336c3
Alexander Nemish <anemish@gmail.com>
2025-06-04 20:48:19 +0200

    fix: DRep serialization
    Borer array-based derivation encodes DReps as 2 element arrays which is incorrect. Return manual codec

--- scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/ledger/TransactionTest.scala
+++ scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/ledger/TransactionTest.scala
@@ -6,7 +6,6 @@ import io.bullet.borer.Cbor
 import org.scalatest.Ignore
 import org.scalatest.funsuite.AnyFunSuite
 import scalus.builtin.{ByteString, PlatformSpecific, given}
-import scalus.utils.Hex.toHex
 
 import java.math.BigInteger
 import java.nio.file.{Files, Path, Paths}
@@ -18,73 +17,72 @@ class TransactionTest extends AnyFunSuite {
 
     private val crypto = summon[PlatformSpecific]
 
-    test("TxnWitnessSet") {
-        asdf()
-        fdsa()
-    }
-
-    private def asdf(): Unit = {
-        val tx = Transaction(
-          KeepRaw(
-            TransactionBody(
-              inputs = Set(
-                TransactionInput(Hash(ByteString.fill(32, 0)), 0)
-              ),
-              outputs = Vector(
-                TransactionOutput.Shelley(
-                  address = AddressBytes.fromBech32(addr),
-                  value = Value.lovelace(2)
+    test("CCL and Scalus transaction hash match") {
+        def scalusTransactionHash = {
+            val tx = Transaction(
+              KeepRaw(
+                TransactionBody(
+                  inputs = Set(
+                    TransactionInput(Hash(ByteString.fill(32, 0)), 0)
+                  ),
+                  outputs = Vector(
+                    TransactionOutput.Shelley(
+                      address = AddressBytes.fromBech32(addr),
+                      value = Value.lovelace(2)
+                    )
+                  ),
+                  fee = Coin(0)
                 )
               ),
-              fee = Coin(0)
+              witnessSet = TransactionWitnessSet(),
+              isValid = true
             )
-          ),
-          witnessSet = TransactionWitnessSet(),
-          isValid = true
-        )
-//        println(tx)
-        println(AddressBytes.fromBech32(addr).toHex)
-        println(Cbor.encode(tx).toByteArray.toHex)
-        val txbody = Cbor.encode(tx.body).toByteArray
-        val txhash = crypto.blake2b_256(ByteString.fromArray(txbody))
-        println(txhash)
-    }
+            //        println(tx)
+            //        println(AddressBytes.fromBech32(addr).toHex)
+            //        println(Cbor.encode(tx).toByteArray.toHex)
+            val txbody = Cbor.encode(tx.body).toByteArray
+            val txhash = crypto.blake2b_256(ByteString.fromArray(txbody))
+            //        println(txhash)
+            txhash.toHex
+        }
 
-    private def fdsa(): Unit = {
-        import com.bloxbean.cardano.client.transaction.spec.*
-        val tx = Transaction
-            .builder()
-            .body(
-              TransactionBody
-                  .builder()
-                  .inputs(
-                    java.util.List.of(
-                      TransactionInput
-                          .builder()
-                          .transactionId(ByteString.fill(32, 0).toHex)
-                          .index(0)
-                          .build()
-                    )
-                  )
-                  .outputs(
-                    java.util.List.of(
-                      TransactionOutput
-                          .builder()
-                          .address(
-                            "addr1qxwg0u9fpl8dac9rkramkcgzerjsfdlqgkw0q8hy5vwk8tzk5pgcmdpe5jeh92guy4mke4zdmagv228nucldzxv95clqe35r3m"
-                          )
-                          .value(Value.builder().coin(BigInteger.valueOf(2)).build())
-                          .build()
-                    )
-                  )
-                  .fee(BigInteger.valueOf(0))
-                  .build()
-            )
-            .build()
-//        println(tx)
-        println(tx.serialize().toHex)
-        val txhash = getTxHash(tx)
-        println(txhash)
+        def cclTransactionHash = {
+            import com.bloxbean.cardano.client.transaction.spec.*
+            val tx = Transaction
+                .builder()
+                .body(
+                  TransactionBody
+                      .builder()
+                      .inputs(
+                        java.util.List.of(
+                          TransactionInput
+                              .builder()
+                              .transactionId(ByteString.fill(32, 0).toHex)
+                              .index(0)
+                              .build()
+                        )
+                      )
+                      .outputs(
+                        java.util.List.of(
+                          TransactionOutput
+                              .builder()
+                              .address(addr)
+                              .value(Value.builder().coin(BigInteger.valueOf(2)).build())
+                              .build()
+                        )
+                      )
+                      .fee(BigInteger.valueOf(0))
+                      .build()
+                )
+                .build()
+            //        println(tx)
+            //        println(tx.serialize().toHex)
+            val txhash = getTxHash(tx)
+            //        println(txhash)
+            txhash
+        }
+
+        assert(scalusTransactionHash == cclTransactionHash)
     }
 
     private val blocksDir = Paths.get(s"../../bloxbean-cardano-client-lib/blocks")
--- scalus-core/shared/src/main/scala/scalus/cardano/ledger/DRep.scala
+++ scalus-core/shared/src/main/scala/scalus/cardano/ledger/DRep.scala
@@ -1,8 +1,7 @@
 package scalus.cardano.ledger
 
-import io.bullet.borer.Codec
 import io.bullet.borer.derivation.ArrayBasedCodecs.*
-import io.bullet.borer.derivation.key
+import io.bullet.borer.*
 
 /** Represents a Delegated Representative (DRep) in the Cardano blockchain.
   *
@@ -12,26 +11,92 @@ import io.bullet.borer.derivation.key
   *   - AlwaysAbstain: A special DRep that always abstains from voting
   *   - AlwaysNoConfidence: A special DRep that always votes no confidence
   */
-enum DRep derives Codec.All:
+enum DRep {
 
     /** A DRep identified by a key hash.
       *
       * @param keyHash
       *   The key hash of the DRep
       */
-    @key(0) case KeyHash(keyHash: AddrKeyHash)
+    case KeyHash(keyHash: AddrKeyHash)
 
     /** A DRep identified by a script hash.
       *
       * @param scriptHash
       *   The script hash of the DRep
       */
-    @key(1) case ScriptHash(scriptHash: scalus.cardano.ledger.ScriptHash)
+    case ScriptHash(scriptHash: scalus.cardano.ledger.ScriptHash)
 
     /** A special DRep that always abstains from voting.
       */
-    @key(2) case AlwaysAbstain
+    case AlwaysAbstain
 
     /** A special DRep that always votes no confidence.
       */
-    @key(3) case AlwaysNoConfidence
+    case AlwaysNoConfidence
+}
+
+object DRep {
+
+    /** CBOR Encoder for DRep. Encodes as a tagged array:
+      *   - [0, keyHash] for KeyHash
+      *   - [1, scriptHash] for ScriptHash
+      *   - [2] for AlwaysAbstain
+      *   - [3] for AlwaysNoConfidence
+      */
+    given Encoder[DRep] with
+        def write(w: Writer, value: DRep): Writer = value match {
+            case DRep.KeyHash(keyHash) =>
+                w.writeArrayOpen(2)
+                    .writeInt(0)
+                    .write(keyHash)
+                    .writeArrayClose()
+
+            case DRep.ScriptHash(scriptHash) =>
+                w.writeArrayOpen(2)
+                    .writeInt(1)
+                    .write(scriptHash)
+                    .writeArrayClose()
+
+            case DRep.AlwaysAbstain =>
+                w.writeArrayOpen(1)
+                    .writeInt(2)
+                    .writeArrayClose()
+
+            case DRep.AlwaysNoConfidence =>
+                w.writeArrayOpen(1)
+                    .writeInt(3)
+                    .writeArrayClose()
+        }
+
+    /** CBOR Decoder for DRep. Decodes from a tagged array:
+      *   - [0, keyHash] for KeyHash
+      *   - [1, scriptHash] for ScriptHash
+      *   - [2] for AlwaysAbstain
+      *   - [3] for AlwaysNoConfidence
+      */
+    given Decoder[DRep] with {
+        def read(r: Reader): DRep = {
+            r.readArrayHeader()
+            val tag = r.readInt()
+
+            val drep = tag match
+                case 0 =>
+                    DRep.KeyHash(r.read[AddrKeyHash]())
+
+                case 1 =>
+                    DRep.ScriptHash(r.read[scalus.cardano.ledger.ScriptHash]())
+
+                case 2 =>
+                    DRep.AlwaysAbstain
+
+                case 3 =>
+                    DRep.AlwaysNoConfidence
+
+                case _ =>
+                    r.validationFailure(s"Invalid DRep type: $tag")
+
+            drep
+        }
+    }
+}
