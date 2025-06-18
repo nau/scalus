a9c963c0ca84f4b850efad348966185f8670ee0b
Alexander Nemish <anemish@gmail.com>
2025-06-18 10:58:08 +0200

    feat: Arbitrary[StakingCredential]

--- scalus-core/shared/src/test/scala/scalus/ledger/api/v1/ArbitraryInstances.scala
+++ scalus-core/shared/src/test/scala/scalus/ledger/api/v1/ArbitraryInstances.scala
@@ -1,6 +1,7 @@
 package scalus.ledger.api.v1
 
 import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Gen
 import scalus.*
 import scalus.prelude.Ord
@@ -13,21 +14,21 @@ trait ArbitraryInstances extends test.ArbitraryInstances {
         Gen.frequency(
           (1, Gen.const(IntervalBoundType.NegInf)),
           (1, Gen.const(IntervalBoundType.PosInf)),
-          (8, Arbitrary.arbitrary[PosixTime].map(IntervalBoundType.Finite(_)))
+          (8, arbitrary[PosixTime].map(IntervalBoundType.Finite(_)))
         )
     }
 
     given Arbitrary[IntervalBound] = Arbitrary {
         for
-            time <- Arbitrary.arbitrary[IntervalBoundType]
-            closure <- Arbitrary.arbitrary[Boolean]
+            time <- arbitrary[IntervalBoundType]
+            closure <- arbitrary[Boolean]
         yield IntervalBound(time, closure)
     }
 
     given Arbitrary[Interval] = Arbitrary {
         for
-            b1 <- Arbitrary.arbitrary[IntervalBound]
-            b2 <- Arbitrary.arbitrary[IntervalBound]
+            b1 <- arbitrary[IntervalBound]
+            b2 <- arbitrary[IntervalBound]
         yield summon[Ord[IntervalBound]].compare(b1, b2) match
             case Ord.Order.Greater => Interval(b2, b1)
             case _                 => Interval(b1, b2)
@@ -43,8 +44,13 @@ trait ArbitraryInstances extends test.ArbitraryInstances {
 
     given Arbitrary[Credential] = Arbitrary {
         Gen.oneOf(
-          Arbitrary.arbitrary[PubKeyHash].map(Credential.PubKeyCredential.apply),
-          Arbitrary.arbitrary[ScriptHash].map(Credential.ScriptCredential.apply)
+          arbitrary[PubKeyHash].map(Credential.PubKeyCredential.apply),
+          arbitrary[ScriptHash].map(Credential.ScriptCredential.apply)
         )
+    }
+
+    given Arbitrary[StakingCredential] = Arbitrary {
+        // We don't generate StakingPtr because it's deprecated and can't be used since Conway era.
+        arbitrary[Credential].map(StakingCredential.StakingHash.apply)
     }
 }
