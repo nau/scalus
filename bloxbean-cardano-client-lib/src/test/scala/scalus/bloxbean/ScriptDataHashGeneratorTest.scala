582c55698dd054dd2ddf977a15c1ce675df4d1f6
Alexander Nemish <anemish@gmail.com>
2025-06-25 22:11:43 +0200

    test: enhance ScriptDataHashGeneratorTest with transaction creation tests

--- bloxbean-cardano-client-lib/src/test/scala/scalus/bloxbean/ScriptDataHashGeneratorTest.scala
+++ bloxbean-cardano-client-lib/src/test/scala/scalus/bloxbean/ScriptDataHashGeneratorTest.scala
@@ -1,24 +1,20 @@
 package scalus.bloxbean
 
-import com.bloxbean.cardano.client.account.Account
-import com.bloxbean.cardano.client.api.model.Amount
-import com.bloxbean.cardano.client.backend.blockfrost.service.BFBackendService
-import com.bloxbean.cardano.client.common.model.Network
-import com.bloxbean.cardano.client.function.helper.SignerProviders
-import com.bloxbean.cardano.client.plutus.spec.CostMdls
-import com.bloxbean.cardano.client.quicktx.{QuickTxBuilder, Tx}
-import com.bloxbean.cardano.client.spec
+import io.bullet.borer.Cbor
 import org.scalatest.funsuite.AnyFunSuite
-import scalus.bloxbean.Interop.toPlutusData
 import scalus.builtin.Data
-import scalus.cardano.ledger
-import scalus.cardano.ledger.*
 import scalus.utils.Hex.toHex
 
+import java.math.BigInteger
 import java.util
 
 class ScriptDataHashGeneratorTest extends AnyFunSuite {
     test("ScriptDataHashGenerator should generate correct hash for empty inputs") {
+        import com.bloxbean.cardano.client.plutus.spec.CostMdls
+        import com.bloxbean.cardano.client.spec
+        import scalus.cardano.ledger
+        import scalus.cardano.ledger.*
+
         val era = ledger.Era.Conway
         val redeemers = Seq.empty[Redeemer]
         val datums = KeepRaw(TaggedSet.empty[Data])
@@ -37,28 +33,108 @@ class ScriptDataHashGeneratorTest extends AnyFunSuite {
         assert(hash.toHex == "9eb0251b2e85b082c3706a3e79b4cf2a2e96f936e912a398591e2486c757f8c1")
     }
 
-/*
-    test("Make transaction") {
-        val backendService = new BFBackendService("http://localhost:8080/api/v1/", "")
+    private def makeBBTx(): String = {
+        import com.bloxbean.cardano.client.account.Account
+        import com.bloxbean.cardano.client.api.UtxoSupplier
+        import com.bloxbean.cardano.client.api.model.Utxo
+        import com.bloxbean.cardano.client.common.ADAConversionUtil.adaToLovelace
+        import com.bloxbean.cardano.client.common.CardanoConstants.LOVELACE
+        import com.bloxbean.cardano.client.common.model.Network
+        import com.bloxbean.cardano.client.function.Output
+        import com.bloxbean.cardano.client.function.helper.InputBuilders
+        import com.bloxbean.cardano.client.transaction
+        import com.bloxbean.cardano.client.transaction.spec.{Transaction, TransactionBody, TransactionOutput, Value}
+
+        //            .txHash("f3b8c0d1e2f3b4c5d6e7f8a9b0c1d2e3f4b5c6d7e8f9a0b1c2d3e4f5g6h7i8j")
         val network = new Network(0, 42)
         val mnemonic =
             "test test test test test test test test test test test test test test test test test test test test test test test sauce"
         val sender = new Account(network, mnemonic)
-        val tx = new Tx()
-            .from(sender.getBaseAddress.getAddress)
-            .payToAddress(sender.baseAddress(), Amount.ada(10))
+        val myaddr =
+            "addr_test1qzwg0u9fpl8dac9rkramkcgzerjsfdlqgkw0q8hy5vwk8tzk5pgcmdpe5jeh92guy4mke4zdmagv228nucldzxv95clq68fray"
+        val output = Output
+            .builder()
+            .address(myaddr)
+            .assetName(LOVELACE)
+            .qty(adaToLovelace(10))
+            .build()
 
-        val quickTxBuilder = QuickTxBuilder(backendService)
-        val signedTx = quickTxBuilder
-            .compose(tx)
-            .withSigner(SignerProviders.signerFrom(sender))
-            .buildAndSign()
+        val txBuilder = output
+            .outputBuilder()
+            .buildInputs(InputBuilders.createFromUtxos(util.List.of(new Utxo()), myaddr))
 
-        println(signedTx)
+        val utxoSupplier: UtxoSupplier = null
+        //        val protocolParamsSupplier: ProtocolParamsSupplier = () => null
+        //        val signedTransaction = TxBuilderContext
+        //            .init(utxoSupplier, protocolParamsSupplier)
+        //            .buildAndSign(txBuilder, signerFrom(sender))
 
-        backendService.getTransactionService.submitTransaction(signedTx.serialize)
+        val body = TransactionBody
+            .builder()
+            .inputs(util.List.of())
+            .outputs(
+              util.List
+                  .of(
+                    TransactionOutput
+                        .builder()
+                        .address(myaddr)
+                        .value(Value.builder.coin(BigInteger.valueOf(1234234)).build())
+                        .build()
+                  )
+            )
+            .fee(adaToLovelace(2))
+            .ttl(1000)
+            .build()
+        val transaction = new Transaction
+        transaction.setEra(com.bloxbean.cardano.client.spec.Era.Conway)
+        transaction.setBody(body)
+        transaction.setValid(true)
+        transaction.serialize.toHex
     }
-*/
 
-    // Additional tests can be added here to cover more scenarios
+    private def makeScalusTx(): String = {
+        import scalus.cardano.address.Address
+        import scalus.cardano.ledger.*
+
+        // Create transaction components
+        val myaddr =
+            "addr_test1qzwg0u9fpl8dac9rkramkcgzerjsfdlqgkw0q8hy5vwk8tzk5pgcmdpe5jeh92guy4mke4zdmagv228nucldzxv95clq68fray"
+        val address = Address.fromBech32(myaddr)
+
+        // Create empty input set
+        val inputs = Set.empty[TransactionInput]
+
+        // Create an output with lovelace (ADA)
+        val output = TransactionOutput.Shelley(
+          address = address,
+          value = Value(Coin(1234234))
+        )
+
+        // Create transaction body
+        val txBody = TransactionBody(
+          inputs = inputs,
+          outputs = IndexedSeq(output),
+          fee = Coin(2_000000), // 2 ADA in lovelace
+          ttl = Some(1000)
+        )
+
+        // Wrap body in KeepRaw to preserve CBOR encoding
+        val wrappedBody = KeepRaw(txBody)
+
+        // Create the transaction
+        val transaction = Transaction(
+          body = wrappedBody,
+          witnessSet = TransactionWitnessSet(),
+          isValid = true
+        )
+
+        // Serialize to CBOR and convert to hex
+        Cbor.encode(transaction).toByteArray.toHex
+    }
+
+    test("Make transaction") {
+        println(makeBBTx())
+        println(makeScalusTx())
+        assert(makeBBTx() == makeScalusTx())
+    }
 }
