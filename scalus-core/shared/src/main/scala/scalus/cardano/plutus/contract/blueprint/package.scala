db1fd3c4b6c75d7a1c7ebe10b67bb7aca6751ed1
otto <sae3023@gmail.com>
2025-07-17 14:50:29 +0300

    Use the `Script` as an entry point to the blueprint generation, simplify

--- scalus-core/shared/src/main/scala/scalus/cardano/plutus/contract/blueprint/package.scala
+++ scalus-core/shared/src/main/scala/scalus/cardano/plutus/contract/blueprint/package.scala
@@ -1,9 +1,7 @@
 package scalus.cardano.plutus.contract
-import io.bullet.borer.Cbor
 import scalus.buildinfo.BuildInfo
-import scalus.builtin.{platform, ByteString}
-import scalus.cardano.ledger.Language
-import scalus.cardano.ledger.Language.languageId
+import scalus.cardano.ledger.Script.{PlutusV1, PlutusV2, PlutusV3}
+import scalus.cardano.ledger.{Language, Script}
 import scalus.utils.Hex.toHex
 
 package object blueprint {
@@ -16,40 +14,38 @@ package object blueprint {
       *   the title of the "blueprintee" contract
       * @param description
       *   the description of the "blueprintee" contact
-      * @param version
-      *   the plutus version used in the contract
-      * @param validatorFnCbored
-      *   CBORed UPLC of the validator
+      * @param validatorScript
+      *   the script of the validator
       */
     def mkBlueprint(
         contractTitle: String,
         description: String,
-        version: Language,
-        validatorFnCbored: Array[Byte]
+        validatorScript: Script
     ): Blueprint = {
-        val preamble = mkPreamble(contractTitle, description, version)
-        val blueprintValidator = mkValidator(version, validatorFnCbored)
+        val preamble = mkPreamble(contractTitle, description, validatorScript.language)
+        val blueprintValidator = mkValidator(validatorScript)
         Blueprint(preamble, Seq(blueprintValidator))
     }
 
-    private def mkPreamble(title: String, description: String, version: Language) =
+    private def mkPreamble(title: String, description: String, version: Option[Language]) =
         Preamble(
           title = title,
           description = Some(description),
           compiler = Some(CompilerInfo("scalus", Some(BuildInfo.scalusVersion))),
-          plutusVersion = Some(version)
+          plutusVersion = version
         )
 
-    private inline def mkValidator(version: Language, cboredValidatorFn: Array[Byte]) = {
-        val decodedFn =
-            Cbor.decode(cboredValidatorFn).to[Array[Byte]].value // assumed to be flattened
-        val preimage = languageId(version).toByte +: decodedFn
-        val hash = platform.blake2b_224(ByteString.fromArray(preimage)).toHex
-
+    private def mkValidator(validatorScript: Script) = {
+        val cbor = validatorScript match {
+            case PlutusV1(script)      => script.toHex
+            case PlutusV2(script)      => script.toHex
+            case PlutusV3(script)      => script.toHex
+            case Script.Native(script) => script.toCbor.toHex
+        }
         Validator(
           "validator",
-          compiledCode = Some(cboredValidatorFn.toHex),
-          hash = Some(hash)
+          compiledCode = Some(cbor),
+          hash = Some(validatorScript.scriptHash.toHex)
         )
     }
 }
--- scalus-core/shared/src/test/scala/scalus/cardano/plutus/contract/blueprint/BlueprintTest.scala
+++ scalus-core/shared/src/test/scala/scalus/cardano/plutus/contract/blueprint/BlueprintTest.scala
@@ -1,38 +1,14 @@
 package scalus.cardano.plutus.contract.blueprint
-import io.bullet.borer.Cbor
 import org.scalatest.funsuite.AnyFunSuite
-import scalus.builtin.{platform, ByteString}
-import scalus.cardano.ledger.Language.{languageId, PlutusV2, PlutusV3}
 import scalus.cardano.plutus.contract.blueprint
-import scalus.cardano.plutus.contract.blueprint.ValidBlueprints.ForFakeValidator.compiledFakeValidatorFn
-import scalus.utils.Hex
 
 class BlueprintTest extends AnyFunSuite {
 
-    test("should hash the script using the language tag") {
-        val bp = blueprint.mkBlueprint("title", "description", PlutusV3, compiledFakeValidatorFn)
-        val validator = bp.validators.head
-
-        val hash = validator.hash.get
-        val compiled = validator.compiledCode.get
-
-        val unCboredCompiledScript = Cbor.decode(Hex.hexToBytes(compiled)).to[Array[Byte]].value
-        val assumedPreimage = languageId(PlutusV3).toByte +: unCboredCompiledScript
-        val expectedHash = platform.blake2b_224(ByteString.fromArray(assumedPreimage))
-
-        assert(expectedHash.toHex == hash)
-
-        val incorrectPreimage = languageId(PlutusV2).toByte +: unCboredCompiledScript
-        val v2Hash = platform.blake2b_224(ByteString.fromArray(incorrectPreimage))
-
-        assert(expectedHash != v2Hash)
-    }
-
     test("should produce a correct JSON value") {
         val title = "title"
-        val descriptions = "description"
-        val bp = blueprint.mkBlueprint(title, descriptions, PlutusV3, compiledFakeValidatorFn)
-        val expected = ValidBlueprints.ForFakeValidator.compiledAndHashed(title, descriptions)
+        val description = "description"
+        val bp = blueprint.mkBlueprint(title, description, emptyScript)
+        val expected = blueprintedScript(title, description)
 
         assert(bp.show == expected)
     }
