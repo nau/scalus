2583930a24340a1c52fc357ce00b3c390bbd4875
Otto Edgar <68739556+sae3023@users.noreply.github.com>
2025-07-21 13:38:04 +0300

    blueprint builder API
    more end-to-endy blueprints

--- scalus-core/shared/src/test/scala/scalus/cardano/plutus/contract/blueprint/blueprints.scala
+++ scalus-core/shared/src/test/scala/scalus/cardano/plutus/contract/blueprint/blueprints.scala
@@ -1,5 +1,6 @@
 package scalus.cardano.plutus.contract.blueprint
 import scalus.Compiler.compile
+import scalus.buildinfo.BuildInfo
 import scalus.builtin.{ByteString, Data}
 import scalus.cardano.ledger.Script
 import scalus.ledger.api.v1.PosixTime
@@ -8,7 +9,7 @@ import scalus.{plutusV3, toUplc}
 private val bytes = compile((ctx: Data) => ()).toUplc().plutusV3.cborEncoded
 val emptyScript = Script.PlutusV3(ByteString.fromArray(bytes))
 def blueprintedScript(title: String, description: String) =
-    s"""{"preamble":{"title":"$title","description":"$description","compiler":{"name":"scalus","version":"0.10.0"},"plutusVersion":"v3"},"validators":[{"title":"validator","compiledCode":"450101002499","hash":"186e32faa80a26810392fda6d559c7ed4721a65ce1c9d4ef3e1c87b4"}]}""".stripMargin
+    s"""{"preamble":{"title":"$title","description":"$description","compiler":{"name":"scalus","version":"${BuildInfo.version}"},"plutusVersion":"v3"},"validators":[{"title":"validator","compiledCode":"450101002499","hash":"186e32faa80a26810392fda6d559c7ed4721a65ce1c9d4ef3e1c87b4"}]}""".stripMargin
 
 enum Interval {
     case Finite(value: Int)
