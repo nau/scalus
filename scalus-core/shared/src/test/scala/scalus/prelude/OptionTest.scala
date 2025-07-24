2f9c8c1d6c57d785738ab8753c7e6b4f1996546e
otto <sae3023@gmail.com>
2025-07-24 18:07:48 +0300

    ord tests

--- scalus-core/shared/src/test/scala/scalus/prelude/OptionTest.scala
+++ scalus-core/shared/src/test/scala/scalus/prelude/OptionTest.scala
@@ -4,16 +4,41 @@ import scalus.*
 import scalus.prelude.given
 import scalus.sir.SIR
 import scalus.uplc.Term
+import prelude.Option as ScalusOption
 
 import scala.language.implicitConversions
 
 class OptionTest extends StdlibTestKit {
     test("eq") {
-        assert((prelude.Option.empty[String]) === prelude.Option.None)
-        assert(prelude.Option.Some(BigInt(1)) === prelude.Option.Some(BigInt(1)))
-        assert(prelude.Option.Some(BigInt(1)) !== prelude.Option.Some(BigInt(2)))
-        assertEval(prelude.Option.Some(true) !== prelude.Option.None)
-        assertEval(prelude.Option.Some(true) === prelude.Option.Some(true))
-        assertEval(prelude.Option.Some(true) !== prelude.Option.Some(false))
+        assert((ScalusOption.empty[String]) === ScalusOption.None)
+        assert(ScalusOption.Some(BigInt(1)) === ScalusOption.Some(BigInt(1)))
+        assert(ScalusOption.Some(BigInt(1)) !== ScalusOption.Some(BigInt(2)))
+        assertEval(ScalusOption.Some(true) !== ScalusOption.None)
+        assertEval(ScalusOption.Some(true) === ScalusOption.Some(true))
+        assertEval(ScalusOption.Some(true) !== ScalusOption.Some(false))
+    }
+
+    test("ord") {
+        import prelude.OrdCompanion.given
+        import ScalusOption.given
+        check { (pair: (Int, Int)) =>
+            val (left, right) = pair
+            val leftOpt = prelude.Option(BigInt(left))
+            val rightOpt = prelude.Option(BigInt(right))
+            leftOpt.gt(ScalusOption.empty) && rightOpt.gt(
+              ScalusOption.empty
+            ) && (if left > right then {
+                      leftOpt gt rightOpt
+                  } else if left < right then {
+                      leftOpt lt rightOpt
+                  } else leftOpt equiv rightOpt)
+        }
+
+        assertEval(
+          ScalusOption.Some(BigInt(-5)).gt(ScalusOption.None)
+        )
+        assertEval(
+          ScalusOption.Some(BigInt(-5)).gt(ScalusOption.Some(BigInt(-10)))
+        )
     }
 }
