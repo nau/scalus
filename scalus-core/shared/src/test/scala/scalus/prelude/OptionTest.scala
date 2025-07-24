febe24acfdab4486c28cc2722d4c663093cb8a11
otto <sae3023@gmail.com>
2025-07-24 17:24:36 +0300

    use testkit in option tests

--- scalus-core/shared/src/test/scala/scalus/prelude/OptionTest.scala
+++ scalus-core/shared/src/test/scala/scalus/prelude/OptionTest.scala
@@ -1,31 +1,19 @@
 package scalus.prelude
 
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import scalus.Compiler.compile
 import scalus.*
-import scalus.prelude.{*, given}
+import scalus.prelude.given
 import scalus.sir.SIR
 import scalus.uplc.Term
-import scalus.uplc.TermDSL.given
-import scalus.uplc.eval.PlutusVM
-import scalus.uplc.test.ArbitraryInstances
 
 import scala.language.implicitConversions
 
-class OptionTest extends AnyFunSuite with ScalaCheckPropertyChecks with ArbitraryInstances {
-    private given PlutusVM = PlutusVM.makePlutusV2VM()
+class OptionTest extends StdlibTestKit {
     test("eq") {
-        assert((prelude.Option.None: prelude.Option[String]) === prelude.Option.None)
+        assert((prelude.Option.empty[String]) === prelude.Option.None)
         assert(prelude.Option.Some(BigInt(1)) === prelude.Option.Some(BigInt(1)))
         assert(prelude.Option.Some(BigInt(1)) !== prelude.Option.Some(BigInt(2)))
-        assertEval(compile(prelude.Option.Some(true) === prelude.Option.None), false)
-        assertEval(compile(prelude.Option.Some(true) === prelude.Option.Some(true)), true)
-        assertEval(compile(prelude.Option.Some(true) !== prelude.Option.Some(true)), false)
-    }
-
-    private def assertEval(sir: SIR, expected: Term) = {
-        val term = sir.toUplc()
-        assert(term.evaluate == expected)
+        assertEval(prelude.Option.Some(true) !== prelude.Option.None)
+        assertEval(prelude.Option.Some(true) === prelude.Option.Some(true))
+        assertEval(prelude.Option.Some(true) !== prelude.Option.Some(false))
     }
 }
