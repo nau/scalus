56df1dc84fdb867860674f58a7f878eddc5d0c63
otto <sae3023@gmail.com>
2025-08-11 14:36:01 +0200

    fmt

--- scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/ledger/txbuilder/TxBuilderTest.scala
+++ scalus-cardano-ledger/jvm/src/test/scala/scalus/cardano/ledger/txbuilder/TxBuilderTest.scala
@@ -310,26 +310,27 @@ class TxBuilderTest
         }
     }
 
-    private lazy val fullSuiteValidator: Validator { type Error = TransactionException } = new Validator {
-        override type Error = TransactionException
-        override def validate(context: Context, state: State, event: Event): Result =
-            for
-                _ <- EmptyInputsValidator.validate(context, state, event)
-                _ <- InputsAndReferenceInputsDisjointValidator.validate(context, state, event)
-                _ <- AllInputsMustBeInUtxoValidator.validate(context, state, event)
-                _ <- ValueNotConservedUTxOValidator.validate(context, state, event)
-                _ <- VerifiedSignaturesInWitnessesValidator.validate(context, state, event)
-                _ <- MissingKeyHashesValidator.validate(context, state, event)
-                _ <- MissingOrExtraScriptHashesValidator.validate(context, state, event)
-                _ <- NativeScriptsValidator.validate(context, state, event)
-                _ <- TransactionSizeValidator.validate(context, state, event)
-                _ <- FeesOkValidator.validate(context, state, event)
-                _ <- OutputsHaveNotEnoughCoinsValidator.validate(context, state, event)
-                _ <- OutputsHaveTooBigValueStorageSizeValidator.validate(context, state, event)
-                _ <- OutsideValidityIntervalValidator.validate(context, state, event)
-                _ <- OutsideForecastValidator.validate(context, state, event)
-                _ <- ExUnitsTooBigValidator.validate(context, state, event)
-                _ <- TooManyCollateralInputsValidator.validate(context, state, event)
-            yield ()
-    }
+    private lazy val fullSuiteValidator: Validator { type Error = TransactionException } =
+        new Validator {
+            override type Error = TransactionException
+            override def validate(context: Context, state: State, event: Event): Result =
+                for
+                    _ <- EmptyInputsValidator.validate(context, state, event)
+                    _ <- InputsAndReferenceInputsDisjointValidator.validate(context, state, event)
+                    _ <- AllInputsMustBeInUtxoValidator.validate(context, state, event)
+                    _ <- ValueNotConservedUTxOValidator.validate(context, state, event)
+                    _ <- VerifiedSignaturesInWitnessesValidator.validate(context, state, event)
+                    _ <- MissingKeyHashesValidator.validate(context, state, event)
+                    _ <- MissingOrExtraScriptHashesValidator.validate(context, state, event)
+                    _ <- NativeScriptsValidator.validate(context, state, event)
+                    _ <- TransactionSizeValidator.validate(context, state, event)
+                    _ <- FeesOkValidator.validate(context, state, event)
+                    _ <- OutputsHaveNotEnoughCoinsValidator.validate(context, state, event)
+                    _ <- OutputsHaveTooBigValueStorageSizeValidator.validate(context, state, event)
+                    _ <- OutsideValidityIntervalValidator.validate(context, state, event)
+                    _ <- OutsideForecastValidator.validate(context, state, event)
+                    _ <- ExUnitsTooBigValidator.validate(context, state, event)
+                    _ <- TooManyCollateralInputsValidator.validate(context, state, event)
+                yield ()
+        }
 }
