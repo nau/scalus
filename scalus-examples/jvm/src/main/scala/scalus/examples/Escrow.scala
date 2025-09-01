15aeda61763870ab50b34c54b0fb9266058673a9
Nikzed <nik.shadkov@gmail.com>
2025-09-01 15:11:53 +0300

    feat: Escrow and EscrowOffchain added

---
+++ scalus-examples/jvm/src/main/scala/scalus/examples/Escrow.scala
@@ -0,0 +1,242 @@
+package scalus.examples
+
+import scalus.{show as _, *}
+import scalus.prelude.Show.*
+import scalus.ledger.api.v3.*
+import scalus.prelude.*
+import scalus.prelude.log
+import scalus.prelude.Option.*
+import scalus.ledger.api.v1.IntervalBoundType.*
+import scalus.prelude.Eq
+import scalus.ledger.api.v2.OutputDatum
+import scalus.builtin.Data
+import scalus.builtin.Data.{fromData, toData, FromData, ToData}
+import scalus.ledger.api.v1.Value.getLovelace
+import scalus.Compiler.compile
+import scalus.builtin.Builtins.trace
+
+case class EscrowDatum(
+    seller: PubKeyHash,
+    buyer: PubKeyHash,
+    escrowAmount: Lovelace,
+    initializationAmount: Lovelace
+) derives FromData,
+      ToData
+
+@Compile
+object EscrowDatum {
+    given Eq[EscrowDatum] = (x, y) =>
+        x.buyer === y.buyer && x.seller === y.seller &&
+            x.escrowAmount == y.escrowAmount && x.initializationAmount == y.initializationAmount
+}
+
+enum EscrowAction derives FromData, ToData:
+    case Deposit
+    case Pay
+    case Refund
+
+case class EscrowRedeemer(action: EscrowAction) derives FromData, ToData
+
+@Compile
+object EscrowRedeemer
+
+@Compile
+object EscrowUtils {
+
+    def getOutputsByVkh(outputs: List[TxOut], vkh: PubKeyHash): List[TxOut] = {
+        outputs.filter(output =>
+            output.address.credential match {
+                case Credential.PubKeyCredential(pkh) => pkh === vkh
+                case _                                => false
+            }
+        )
+    }
+
+    def getOutputsByAddress(outputs: List[TxOut], addr: Address): List[TxOut] = {
+        outputs.filter(_.address === addr)
+    }
+
+    def getInputsByAddress(inputs: List[TxInInfo], addr: Address): List[TxInInfo] = {
+        inputs.filter(_.resolved.address === addr)
+    }
+
+    def getAdaFromOutputs(outputs: List[TxOut]): Lovelace = {
+        outputs.map(_.value.getLovelace).foldLeft(BigInt(0))(_ + _)
+    }
+
+    def getAdaFromInputs(inputs: List[TxInInfo]): Lovelace = {
+        inputs.map(_.resolved.value.getLovelace).foldLeft(BigInt(0))(_ + _)
+    }
+
+    def mustBeSignedBy(signatories: List[PubKeyHash], vkh: PubKeyHash): Boolean = {
+        signatories.contains(vkh)
+    }
+
+}
+
+@Compile
+object Escrow extends Validator:
+    override def spend(
+        datum: Option[Data],
+        redeemer: Data,
+        txInfo: TxInfo,
+        txOutRef: TxOutRef
+    ): Unit = {
+        log("Escrow script started")
+        val Some(receivedData) = datum: @unchecked
+        val escrowDatum: EscrowDatum = receivedData.to[EscrowDatum]
+        val EscrowRedeemer(action) = redeemer.to[EscrowRedeemer]
+
+        val ownInput = txInfo.inputs
+            .find(input => input.outRef === txOutRef)
+            .get
+            .resolved
+        val contractAddress = ownInput.address
+        val contractInputs = EscrowUtils.getInputsByAddress(txInfo.inputs, contractAddress)
+        val contractBalance = EscrowUtils.getAdaFromInputs(contractInputs)
+
+        log("Contract balance calculation:")
+        log("Number of contract inputs:")
+        log(contractInputs.length.show)
+        log("Contract balance from inputs:")
+        log(contractBalance.show)
+        log("Own input balance:")
+        log(ownInput.value.getLovelace.show)
+        log("Expected total (escrow + init):")
+        log((escrowDatum.escrowAmount + escrowDatum.initializationAmount).show)
+
+        action match {
+            case EscrowAction.Deposit =>
+                handleDeposit(escrowDatum, txInfo, contractAddress, contractBalance, receivedData)
+            case EscrowAction.Pay =>
+                handlePay(escrowDatum, txInfo, contractAddress, contractBalance)
+            case EscrowAction.Refund =>
+                handleRefund(escrowDatum, txInfo, contractAddress, contractBalance)
+        }
+    }
+
+    private def handleDeposit(
+        escrowDatum: EscrowDatum,
+        txInfo: TxInfo,
+        contractAddress: Address,
+        contractBalance: Lovelace,
+        receivedData: Data
+    ): Unit = {
+        log("Handling deposit action")
+
+        require(
+          EscrowUtils.mustBeSignedBy(txInfo.signatories, escrowDatum.buyer),
+          "Buyer must sign deposit transaction"
+        )
+
+        val buyerOutputs = EscrowUtils.getOutputsByVkh(txInfo.outputs, escrowDatum.buyer)
+        val contractOutputs = EscrowUtils.getOutputsByAddress(txInfo.outputs, contractAddress)
+
+        require(contractOutputs.length == BigInt(1), "Expected exactly one contract output")
+        val contractOutput = contractOutputs.head
+
+        require(buyerOutputs.length == BigInt(1), "Expected exactly one buyer output")
+
+        require(
+          contractBalance != escrowDatum.escrowAmount,
+          "Contract must contain only initialization amount before deposit"
+        )
+
+        require(
+          EscrowUtils.getAdaFromOutputs(
+            contractOutputs
+          ) == escrowDatum.escrowAmount + escrowDatum.initializationAmount,
+          "Contract output must contain exactly escrow amount plus initialization amount"
+        )
+
+        contractOutput.datum match {
+            case OutputDatum.OutputDatum(inlineData) =>
+                require(
+                  inlineData == receivedData,
+                  "EscrowDatum must be preserved"
+                )
+            case _ => fail("Expected inline datum")
+        }
+    }
+
+    private def handlePay(
+        escrowDatum: EscrowDatum,
+        txInfo: TxInfo,
+        contractAddress: Address,
+        contractBalance: Lovelace
+    ): Unit = {
+        log("Handling pay action")
+
+        require(
+          contractBalance == escrowDatum.escrowAmount + escrowDatum.initializationAmount,
+          "Contract must be fully funded before payment"
+        )
+
+        val buyerOutputs = EscrowUtils.getOutputsByVkh(txInfo.outputs, escrowDatum.buyer)
+        val sellerOutputs = EscrowUtils.getOutputsByVkh(txInfo.outputs, escrowDatum.seller)
+
+        require(
+          sellerOutputs.nonEmpty,
+          "Seller outputs must not be empty"
+        )
+
+        require(
+          buyerOutputs.nonEmpty,
+          "Buyer outputs must not be empty"
+        )
+
+        require(
+          EscrowUtils.mustBeSignedBy(txInfo.signatories, escrowDatum.buyer),
+          "Only buyer can release payment"
+        )
+
+        require(
+          EscrowUtils.getAdaFromOutputs(
+            sellerOutputs
+          ) == escrowDatum.escrowAmount + escrowDatum.initializationAmount,
+          "Seller must receive exactly escrow amount plus initialization amount"
+        )
+    }
+
+    private def handleRefund(
+        escrowDatum: EscrowDatum,
+        txInfo: TxInfo,
+        contractAddress: Address,
+        contractBalance: Lovelace
+    ): Unit = {
+        log("Handling refund action")
+
+        require(
+          contractBalance == escrowDatum.escrowAmount + escrowDatum.initializationAmount,
+          "Contract must be fully funded before refund"
+        )
+
+        val buyerOutputs = EscrowUtils.getOutputsByVkh(txInfo.outputs, escrowDatum.buyer)
+        val sellerOutputs = EscrowUtils.getOutputsByVkh(txInfo.outputs, escrowDatum.seller)
+
+        require(
+          sellerOutputs.nonEmpty,
+          "Seller outputs must not be empty"
+        )
+
+        require(
+          buyerOutputs.nonEmpty,
+          "Buyer outputs must not be empty"
+        )
+
+        require(
+          EscrowUtils.mustBeSignedBy(txInfo.signatories, escrowDatum.seller),
+          "Only seller can issue refund"
+        )
+
+        require(
+          EscrowUtils.getAdaFromOutputs(buyerOutputs) == escrowDatum.escrowAmount,
+          "Buyer must receive exactly the escrow amount back"
+        )
+    }
+
+object EscrowScript {
+    inline def compiled(using scalus.Compiler.Options) = compile(Escrow.validate)
+    inline def doubleCborHex(using scalus.Compiler.Options) =
+        compiled.toUplc(true).plutusV3.doubleCborHex
+}
---
+++ scalus-examples/jvm/src/main/scala/scalus/examples/EscrowOffchain.scala
@@ -0,0 +1,394 @@
+package scalus.examples
+
+import scalus.bloxbean.SlotConfig
+import com.bloxbean.cardano.client.account.Account
+import com.bloxbean.cardano.client.api.model.{Amount, Result, Utxo}
+import com.bloxbean.cardano.client.backend.api.DefaultProtocolParamsSupplier
+import com.bloxbean.cardano.client.backend.api.DefaultUtxoSupplier
+import com.bloxbean.cardano.client.backend.blockfrost.common.Constants
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFBackendService
+import com.bloxbean.cardano.client.function.helper.*
+import com.bloxbean.cardano.client.quicktx.{QuickTxBuilder, ScriptTx, Tx}
+import com.bloxbean.cardano.client.address.{AddressProvider, Credential}
+import com.bloxbean.cardano.client.common.model.Networks
+import com.bloxbean.cardano.client.common.CardanoConstants.LOVELACE
+import scalus.*
+import scalus.bloxbean.Interop.toPlutusData
+import scalus.bloxbean.ScalusTransactionEvaluator
+import scalus.builtin.{ByteString, Data}
+import com.bloxbean.cardano.client.plutus.spec.PlutusV3Script
+import scalus.builtin.Data.*
+import scalus.builtin.ToData.*
+import scalus.builtin.ToData.toData
+import scalus.ledger.api.v1.PubKeyHash
+
+import java.util.Optional
+import scala.util.control.Breaks.*
+import java.math.BigInteger
+import com.bloxbean.cardano.client.function.TxBuilder
+
+object EscrowOffChain:
+    private val blockfrostApiKey = sys.env("BLOCKFROST_API_KEY")
+    private val network = Networks.preview()
+
+    private val sellerMnemonic = sys.env("SELLER_MNEMONIC")
+    private val buyerMnemonic = sys.env("BUYER_MNEMONIC")
+
+    private val seller = Account(network, sellerMnemonic)
+    private val buyerAccount = Account(network, buyerMnemonic)
+
+    private val backendService =
+        BFBackendService(Constants.BLOCKFROST_PREVIEW_URL, blockfrostApiKey)
+
+    private val utxoSupplier = DefaultUtxoSupplier(backendService.getUtxoService)
+    private val protocolParamsSupplier = DefaultProtocolParamsSupplier(
+      backendService.getEpochService
+    )
+
+    private val script = PlutusV3Script
+        .builder()
+        .cborHex(EscrowScript.doubleCborHex)
+        .build()
+
+    private val scriptAddress = AddressProvider.getEntAddress(script, network)
+
+    private val scriptAddressBech32 = scriptAddress.toBech32()
+
+    inline given scalus.Compiler.Options = scalus.Compiler.Options(
+      targetLoweringBackend = scalus.Compiler.TargetLoweringBackend.SirToUplcV3Lowering,
+      generateErrorTraces = true,
+      optimizeUplc = false,
+      debug = false
+    )
+
+    def initialize(
+        buyerPubKeyHash: ByteString,
+        paymentAmountAda: Long,
+        initialDepositAda: Long = 2L
+    ): Unit = {
+        val sellerPubKeyHash = ByteString.fromArray(seller.hdKeyPair().getPublicKey.getKeyHash)
+        val buyerPKH = PubKeyHash(buyerPubKeyHash)
+        val sellerPKH = PubKeyHash(sellerPubKeyHash)
+        val paymentAmountLovelace = paymentAmountAda * 1_000_000L
+        val initializationAmountLovelace = initialDepositAda * 1_000_000L
+
+        val datum =
+            EscrowDatum(sellerPKH, buyerPKH, paymentAmountLovelace, initializationAmountLovelace)
+
+        val tx = Tx()
+            .from(seller.getBaseAddress.getAddress)
+            .payToContract(
+              scriptAddressBech32,
+              Amount.ada(initialDepositAda),
+              toPlutusData(datum.toData)
+            )
+
+        val signedTx = QuickTxBuilder(backendService)
+            .compose(tx)
+            .withSigner(SignerProviders.signerFrom(seller))
+            .buildAndSign()
+
+        println("Submitting Initialize Escrow TX...")
+        val result: Result[String] =
+            backendService.getTransactionService.submitTransaction(signedTx.serialize)
+        println(s"Result is: ${result}")
+
+        if result.isSuccessful() then {
+            println(s"Initialize Tx submitted successfully: ${result.getValue}")
+            println("Waiting for UTxO to appear at script address...")
+        } else {
+            println(s"Initialize Tx submission failed: ${result.getResponse}")
+            sys.exit(1)
+        }
+    }
+
+    def waitForUtxo(datum: Data): Utxo = {
+        var scriptUtxo: Optional[Utxo] = Optional.empty()
+        var attempts = 0
+        val maxAttempts = 20
+        breakable {
+            while attempts < maxAttempts do {
+                println(s"Searching for UTXO... (Attempt ${attempts + 1}/$maxAttempts)")
+                scriptUtxo = ScriptUtxoFinders.findFirstByDatumHashUsingDatum(
+                  utxoSupplier,
+                  scriptAddressBech32,
+                  toPlutusData(datum)
+                )
+                if scriptUtxo.isPresent then {
+                    println("Found UTXO!")
+                    break
+                }
+                attempts += 1
+                Thread.sleep(15 * 1000)
+            }
+        }
+        scriptUtxo.orElseThrow(() =>
+            new NoSuchElementException(
+              s"UTXO with datum not found at $scriptAddressBech32."
+            )
+        )
+    }
+
+    def waitForUtxoWithAmount(datum: Data, expectedAmount: BigInteger): Utxo = {
+        var scriptUtxo: Optional[Utxo] = Optional.empty()
+        var attempts = 0
+        val maxAttempts = 20
+        breakable {
+            while attempts < maxAttempts do {
+                println(
+                  s"Searching for UTXO with ${expectedAmount} lovelace... (Attempt ${attempts + 1}/$maxAttempts)"
+                )
+
+                val allUtxos = utxoSupplier.getPage(
+                  scriptAddressBech32,
+                  50,
+                  0,
+                  com.bloxbean.cardano.client.api.common.OrderEnum.asc
+                )
+
+                val targetDatum = toPlutusData(datum)
+                scriptUtxo = allUtxos
+                    .stream()
+                    .filter(utxo => {
+
+                        val datumMatches = utxo.getInlineDatum != null &&
+                            utxo.getInlineDatum.equals(targetDatum.serializeToHex())
+
+                        val amountMatches = utxo.getAmount
+                            .stream()
+                            .filter(_.getUnit == LOVELACE)
+                            .findFirst()
+                            .map(_.getQuantity.equals(expectedAmount))
+                            .orElse(false)
+
+                        datumMatches && amountMatches
+                    })
+                    .findFirst()
+
+                if scriptUtxo.isPresent then {
+                    val amount = scriptUtxo
+                        .get()
+                        .getAmount
+                        .stream()
+                        .filter(_.getUnit == LOVELACE)
+                        .findFirst()
+                        .get()
+                        .getQuantity
+                    println(s"Found UTXO with ${amount} lovelace!")
+                    break
+                }
+                attempts += 1
+                Thread.sleep(15 * 1000)
+            }
+        }
+        scriptUtxo.orElseThrow(() =>
+            new NoSuchElementException(
+              s"UTXO with datum and amount ${expectedAmount} not found at $scriptAddressBech32 after $maxAttempts attempts."
+            )
+        )
+    }
+
+    def deposit(buyerAccount: Account, datum: Data, escrowDatum: EscrowDatum): Unit = {
+        // Look for UTXO with only initialization amount (not yet deposited to)
+        val scriptUtxo = waitForUtxoWithAmount(datum, escrowDatum.initializationAmount.bigInteger)
+        println("scriptUtxo: " + scriptUtxo)
+
+        val currentAmount = scriptUtxo.getAmount
+            .stream()
+            .filter(_.getUnit == LOVELACE)
+            .findFirst()
+            .orElseThrow()
+
+        val totalRequiredAmount =
+            escrowDatum.escrowAmount.bigInteger.add(escrowDatum.initializationAmount.bigInteger)
+        val redeemer = EscrowRedeemer(EscrowAction.Deposit)
+
+        val scriptTx = ScriptTx()
+            .collectFrom(scriptUtxo, toPlutusData(redeemer.toData))
+            .payToContract(
+              scriptAddressBech32,
+              Amount.lovelace(totalRequiredAmount),
+              toPlutusData(datum)
+            )
+            .payToAddress(
+              buyerAccount.baseAddress(),
+              Amount.lovelace(BigInteger.valueOf(1000000)) // 1 ADA minimum output for buyer change
+            )
+            .attachSpendingValidator(script)
+
+        val protocolParams = backendService.getEpochService.getProtocolParameters.getValue
+        val currentSlot = backendService.getBlockService.getLatestBlock.getValue.getSlot
+
+        val signedTx = QuickTxBuilder(backendService)
+            .compose(scriptTx)
+            .feePayer(buyerAccount.baseAddress())
+            .withSigner(SignerProviders.signerFrom(buyerAccount))
+            .validFrom(currentSlot)
+            .validTo(currentSlot + 1200)
+            .withRequiredSigners(
+              ByteString.fromArray(buyerAccount.hdKeyPair().getPublicKey.getKeyHash).bytes
+            )
+            .ignoreScriptCostEvaluationError(false)
+            .withTxEvaluator(
+              ScalusTransactionEvaluator(
+                slotConfig = SlotConfig.Preview,
+                protocolParams = protocolParams,
+                utxoSupplier = utxoSupplier
+              )
+            )
+            .buildAndSign()
+
+        println("Submitting Deposit TX...")
+        val result = backendService.getTransactionService.submitTransaction(signedTx.serialize)
+        println(s"Result is: ${result}")
+    }
+
+    def pay(buyerAccount: Account, datum: Data, escrowDatum: EscrowDatum): Unit = {
+        // Look for UTXO with full amount (escrow + initialization)
+        val fullAmount = (escrowDatum.escrowAmount + escrowDatum.initializationAmount).bigInteger
+        val scriptUtxo = waitForUtxoWithAmount(datum, fullAmount)
+        println("scriptUtxo: " + scriptUtxo)
+
+        val contractAmount = scriptUtxo.getAmount
+            .stream()
+            .filter(_.getUnit == LOVELACE)
+            .findFirst()
+            .orElseThrow()
+
+        val redeemer = EscrowRedeemer(EscrowAction.Pay)
+
+        val scriptTx = ScriptTx()
+            .collectFrom(scriptUtxo, toPlutusData(redeemer.toData))
+            .payToAddress(
+              seller.baseAddress(),
+              Amount.lovelace(
+                (escrowDatum.escrowAmount + escrowDatum.initializationAmount).bigInteger
+              )
+            )
+            .payToAddress(
+              AddressProvider
+                  .getEntAddress(
+                    Credential.fromKey(escrowDatum.buyer.hash.bytes),
+                    network
+                  )
+                  .toBech32(),
+              Amount.lovelace(BigInteger.valueOf(1000000)) // 1 ADA minimum output
+            )
+            .attachSpendingValidator(script)
+
+        val protocolParams = backendService.getEpochService.getProtocolParameters.getValue
+        val currentSlot = backendService.getBlockService.getLatestBlock.getValue.getSlot
+
+        val signedTx = QuickTxBuilder(backendService)
+            .compose(scriptTx)
+            .feePayer(buyerAccount.baseAddress())
+            .withSigner(SignerProviders.signerFrom(buyerAccount))
+            .validFrom(currentSlot)
+            .validTo(currentSlot + 1200)
+            .withRequiredSigners(
+              ByteString.fromArray(buyerAccount.hdKeyPair().getPublicKey.getKeyHash).bytes
+            )
+            .ignoreScriptCostEvaluationError(false)
+            .withTxEvaluator(
+              ScalusTransactionEvaluator(
+                slotConfig = SlotConfig.Preview,
+                protocolParams = protocolParams,
+                utxoSupplier = utxoSupplier
+              )
+            )
+            .buildAndSign()
+
+        println("Submitting Pay TX...")
+        val result = backendService.getTransactionService.submitTransaction(signedTx.serialize)
+        println(s"Result is: ${result}")
+    }
+
+    def refund(datum: Data, escrowDatum: EscrowDatum, buyerAddress: String): Unit = {
+        // Look for UTXO with full amount (escrow + initialization)
+        val fullAmount = (escrowDatum.escrowAmount + escrowDatum.initializationAmount).bigInteger
+        val scriptUtxo = waitForUtxoWithAmount(datum, fullAmount)
+        println("scriptUtxo: " + scriptUtxo)
+
+        val contractAmount = scriptUtxo.getAmount
+            .stream()
+            .filter(_.getUnit == LOVELACE)
+            .findFirst()
+            .orElseThrow()
+
+        val redeemer = EscrowRedeemer(EscrowAction.Refund)
+
+        val scriptTx = ScriptTx()
+            .collectFrom(scriptUtxo, toPlutusData(redeemer.toData))
+            .payToAddress(
+              buyerAddress,
+              Amount.lovelace(escrowDatum.escrowAmount.bigInteger)
+            )
+            .payToAddress(
+              seller.baseAddress(),
+              Amount.lovelace(escrowDatum.initializationAmount.bigInteger)
+            )
+            .attachSpendingValidator(script)
+
+        val protocolParams = backendService.getEpochService.getProtocolParameters.getValue
+        val currentSlot = backendService.getBlockService.getLatestBlock.getValue.getSlot
+
+        val sellerPubKeyHash = ByteString.fromArray(seller.hdKeyPair().getPublicKey.getKeyHash)
+
+        val signedTx = QuickTxBuilder(backendService)
+            .compose(scriptTx)
+            .feePayer(seller.baseAddress())
+            .withSigner(SignerProviders.signerFrom(seller))
+            .validFrom(currentSlot)
+            .validTo(currentSlot + 1200)
+            .withRequiredSigners(sellerPubKeyHash.bytes)
+            .ignoreScriptCostEvaluationError(false)
+            .withTxEvaluator(
+              ScalusTransactionEvaluator(
+                slotConfig = SlotConfig.Preview,
+                protocolParams = protocolParams,
+                utxoSupplier = utxoSupplier
+              )
+            )
+            .buildAndSign()
+
+        println("Submitting Refund TX...")
+        val result = backendService.getTransactionService.submitTransaction(signedTx.serialize)
+        println(s"Result is: ${result}")
+    }
+
+    def main(args: Array[String]): Unit = {
+        val buyerPubKeyHash = ByteString.fromArray(buyerAccount.hdKeyPair().getPublicKey.getKeyHash)
+
+        val paymentAmountAda = 10L
+        val initialDepositAda = 2L
+
+        val sellerPubKeyHash = ByteString.fromArray(seller.hdKeyPair().getPublicKey.getKeyHash)
+        val buyerPKH = PubKeyHash(buyerPubKeyHash)
+        val sellerPKH = PubKeyHash(sellerPubKeyHash)
+        val paymentAmountLovelace = paymentAmountAda * 1_000_000L
+        val initializationAmountLovelace = initialDepositAda * 1_000_000L
+
+        val datum =
+            EscrowDatum(sellerPKH, buyerPKH, paymentAmountLovelace, initializationAmountLovelace)
+
+        println(s"Escrow Datum:")
+        println(s"  Buyer: ${buyerPKH}")
+        println(s"  Seller: ${sellerPKH}")
+        println(s"  Payment Amount: ${paymentAmountLovelace} lovelace")
+        println(s"  Initial Deposit: ${initialDepositAda} ADA")
+
+        println("Step 1: Seller initializes escrow contract")
+        initialize(buyerPubKeyHash, paymentAmountAda, initialDepositAda)
+        Thread.sleep(45 * 1000)
+
+        println("Step 2: Buyer deposits payment")
+        deposit(buyerAccount, datum.toData, datum)
+        println("Waiting longer for deposit transaction to be confirmed...")
+        Thread.sleep(120 * 1000)
+
+        println("Step 3: Buyer releases payment to seller")
+        pay(buyerAccount, datum.toData, datum)
+
+        // println("Step 3: Seller refunds to buyer")
+        // refund(datum.toData, datum, buyerAccount.baseAddress())
+    }
