cf9b23c18b7afb3dbe341e9e6d97dfd5c257e15a
Alexander Nemish <anemish@gmail.com>
2025-09-20 16:07:58 +0200

    refactor: remove deprecated methods from TermDSL

--- scalus-core/shared/src/main/scala/scalus/uplc/TermDSL.scala
+++ scalus-core/shared/src/main/scala/scalus/uplc/TermDSL.scala
@@ -5,36 +5,11 @@ import scalus.builtin.Data
 import scala.collection.immutable
 
 object TermDSL:
-    @deprecated("Use Term.applyToList instead", "0.9.0")
-    def applyToList(app: Term): (Term, immutable.List[Term]) =
-        app match
-            case Term.Apply(f, arg) =>
-                val (f1, args) = applyToList(f)
-                (f1, args :+ arg)
-            case f => (f, Nil)
-
-    @deprecated("Use Term.λ instead", "0.9.0")
-    def λ(names: String*)(term: Term): Term = lam(names*)(term)
-    @deprecated("Use Term.λλ instead", "0.9.0")
-    def λλ(name: String)(f: Term => Term): Term = lam(name)(f(vr(name)))
-    @deprecated("Use Term.lam instead", "0.9.0")
-    def lam(names: String*)(term: Term): Term = names.foldRight(term)(Term.LamAbs(_, _))
-    @deprecated("Use Term.vr instead", "0.9.0")
-    def vr(name: String): Term = Term.Var(NamedDeBruijn(name))
-
-    extension (sc: StringContext)
-        @deprecated("Use Term.vr instead", "0.9.0")
-        def vr(args: Any*): Term = Term.Var(NamedDeBruijn(sc.parts.head))
-
     given Conversion[DefaultFun, Term] with
         def apply(bn: DefaultFun): Term = Term.Builtin(bn)
 
     given constantAsTerm[A: Constant.LiftValue]: Conversion[A, Term] with
         def apply(c: A): Term = Term.Const(summon[Constant.LiftValue[A]].lift(c))
-
-    extension [A: Constant.LiftValue](a: A)
-        @deprecated("Use Term.asTerm instead", "0.9.0")
-        def asTerm: Term = Term.Const(summon[Constant.LiftValue[A]].lift(a))
 
     given Conversion[Constant, Term] with
         def apply(c: Constant): Term = Term.Const(c)
