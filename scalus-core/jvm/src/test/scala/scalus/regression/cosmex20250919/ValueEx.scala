a9d4b139b67f7e13a436a8daa57f404116ab2a82
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2025-10-03 18:29:42 +0300

    added forgotten files, needs for compilation

---
+++ scalus-core/jvm/src/test/scala/scalus/regression/cosmex20250919/ValueEx.scala
@@ -0,0 +1,56 @@
+package scalus.regression.cosmex20250919
+
+import scalus.prelude.*
+import scalus.ledger.api.v3.*
+import scalus.*
+
+@Compile
+object ValueEx {
+
+    def unionVal(
+        l: Value,
+        r: Value
+    ): SortedMap[CurrencySymbol, SortedMap[TokenName, These[BigInt, BigInt]]] = {
+        val combined: SortedMap[
+          CurrencySymbol,
+          These[SortedMap[TokenName, BigInt], SortedMap[TokenName, BigInt]]
+        ] = SortedMap.union(l.toSortedMap, r.toSortedMap)
+        combined.mapValues {
+            case These.These(v1, v2) => SortedMap.union(v1, v2)
+            case These.This(v1) =>
+                v1.mapValues {
+                    These.This(_)
+                }
+            case These.That(v2) =>
+                v2.mapValues {
+                    These.That(_)
+                }
+        }
+    }
+
+    def unionWith(op: (BigInt, BigInt) => BigInt)(a: Value, b: Value): Value = {
+        val combined = unionVal(a, b)
+        val unThese: These[BigInt, BigInt] => BigInt = {
+            case These.These(v1, v2) => op(v1, v2)
+            case These.This(v1)      => op(v1, 0)
+            case These.That(v2)      => op(0, v2)
+        }
+
+        Value.fromList(
+          // SortedMap.unsafeFromList(
+          combined.toList.filterMap { pair =>
+              val tokens = pair._2.toList.filterMap { case (tn, v) =>
+                  val value = unThese(v)
+                  if value !== BigInt(0) then Option.Some((tn, value))
+                  else Option.None
+              }
+
+              // if tokens.nonEmpty then Option.Some((pair._1, SortedMap.unsafeFromList(tokens)))
+              if tokens.nonEmpty then Option.Some((pair._1, tokens))
+              else Option.None
+          }
+          // )
+        )
+    }
+
+}
