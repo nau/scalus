bf8a3498afcf2604a1ff13884dc940beab8cec6d
rhulenko <roman.hulenko.job@gmail.com>
2025-10-14 16:43:18 +0300

    Refactor HtlcValidatorTest and return deleted tests; Extract some tests to HtlcTxEvaluatorTest;

--- scalus-examples/jvm/src/test/scala/scalus/examples/htlc/HtlcValidatorTest.scala
+++ scalus-examples/jvm/src/test/scala/scalus/examples/htlc/HtlcValidatorTest.scala
@@ -1,17 +1,7 @@
 package scalus.examples.htlc
 
-import com.bloxbean.cardano.client.account.Account
-import com.bloxbean.cardano.client.address.AddressProvider
-import com.bloxbean.cardano.client.api.util.CostModelUtil
-import com.bloxbean.cardano.client.common.ADAConversionUtil
-import com.bloxbean.cardano.client.common.model.Networks
-import com.bloxbean.cardano.client.plutus.spec.{Redeemer, *}
-import com.bloxbean.cardano.client.transaction.spec
-import com.bloxbean.cardano.client.transaction.spec.*
-import com.bloxbean.cardano.client.transaction.util.TransactionUtil
 import org.scalatest.funsuite.AnyFunSuite
 import scalus.*
-import scalus.bloxbean.{Interop, SlotConfig, TxEvaluator}
 import scalus.builtin.Builtins.sha3_256
 import scalus.builtin.Data.toData
 import scalus.builtin.{ByteString, Data}
@@ -21,12 +11,10 @@ import scalus.testkit.ScalusTest
 import scalus.uplc.*
 import scalus.uplc.eval.*
 
-import java.math.BigInteger
-import java.util
 import scala.util.Try
 
 class HtlcValidatorTest extends AnyFunSuite with ScalusTest {
-    import Person.*
+    import HtlcValidatorTest.*
 
     test("committer successfully unlocks HTLC after timeout") {
         TestCase(
@@ -38,13 +26,13 @@ class HtlcValidatorTest extends AnyFunSuite with ScalusTest {
           validFrom = 1000,
           signatories = List(Committer),
           action = Action.Timeout,
-          preimage = genByteStringOfN(32).sample.get,
+          preimage = generatePreimage(),
           expected = success
         ).runWithDebug()
     }
 
     test("receiver successfully unlocks HTLC with valid preimage before timeout") {
-        val preimage = genByteStringOfN(32).sample.get
+        val preimage = generatePreimage()
         TestCase(
           inputs = List(Receiver gives 100),
           outputs = List(Receiver gets 1050),
@@ -69,162 +57,86 @@ class HtlcValidatorTest extends AnyFunSuite with ScalusTest {
           validFrom = 10,
           signatories = List(Committer),
           action = Action.Timeout,
-          preimage = genByteStringOfN(32).sample.get,
-          expected = failure("Committer Transaction must be exclusively after timeout")
+          preimage = generatePreimage(),
+          expected = failure(InvalidCommitterTimePoint)
         ).runWithDebug()
     }
 
-    test("successfully unlock HTLC with valid preimage") {
-        val preimage = genByteStringOfN(32).sample.get
-        val image = sha3_256(preimage)
-        val contractDatum = ContractDatum(
-          committer = Committer.pkh,
-          receiver = Receiver.pkh,
-          image = image,
-          timeout = 1745261347000L
-        )
-
-        val txid = random[TxId].hash.toHex
-        val htlcInput = new TransactionInput(txid, 0)
-        val scriptRefInput = new TransactionInput(txid, 1)
-
-        // Use a transaction builder to create the transaction
-        val tx = makeUnlockingTransaction(
-          input = htlcInput,
-          scriptRefInput = scriptRefInput,
-          startTimeMillis = 1745261346000L,
+    test("receiver can't unlock HTLC after timeout") {
+        val preimage = generatePreimage()
+        TestCase(
+          inputs = List(Receiver gives 100),
+          outputs = List(Receiver gets 1050),
+          value = 1000,
+          fee = 50,
+          timeout = 1000,
+          validFrom = 1500,
+          signatories = List(Receiver),
           action = Action.Reveal(preimage),
-          signatories = Seq(PubKeyHash(Receiver.pkh))
-        )
-
-        val datum = contractDatum.toData
-
-        val (utxos: Map[TransactionInput, TransactionOutput], context: ScriptContext) =
-            makeContext(htlcInput, scriptRefInput, tx, datum)
-
-        // run as Scala function
-        HtlcValidator.validate(context.toData)
-
-        // run as UPLC script
-        checkResult(expected = success, actual = script.runWithDebug(context))
-
-        // Use Scalus TxEvaluator calculate redeemers/costs/fees for the transaction
-        val evaluator = TxEvaluator(
-          SlotConfig.Mainnet,
-          initialBudget = ExBudget.fromCpuAndMemory(10_000000000L, 10_000000L),
-          protocolMajorVersion = 10,
-          costMdls = costMdls
-        )
-        // Runs Cardano Transaction Validation Ledger rules
-        val redeemers = evaluator.evaluateTx(tx, utxos)
-        assert(redeemers.size == 1)
+          preimage = preimage,
+          expected = failure(InvalidReceiverTimePoint)
+        ).runWithDebug()
     }
 
-    private def makeContext(
-        htlcInput: TransactionInput,
-        scriptRefInput: TransactionInput,
-        tx: Transaction,
-        datum: ChangedParameters
-    ) = {
-        import Interop.*
-        val payeeAddress = sender.baseAddress()
-
-        val plutusScript = PlutusV3Script
-            .builder()
-            .`type`("PlutusScriptV3")
-            .cborHex(script.doubleCborHex)
-            .build()
-            .asInstanceOf[PlutusV3Script]
-        val scriptRefUtxo = Map(
-          scriptRefInput -> TransactionOutput
-              .builder()
-              .value(spec.Value.builder().coin(BigInteger.valueOf(20)).build())
-              .address(payeeAddress)
-              .scriptRef(
-                plutusScript
-              )
-              .build()
-        )
-        val htlcAddress =
-            AddressProvider.getBaseAddress(plutusScript, plutusScript, Networks.mainnet())
-        val htlcUtxo = Map(
-          htlcInput -> TransactionOutput
-              .builder()
-              .value(spec.Value.builder().coin(BigInteger.valueOf(20)).build())
-              .address(htlcAddress.getAddress)
-              .inlineDatum(toPlutusData(datum))
-              .build()
-        )
-
-        val utxos = htlcUtxo ++ scriptRefUtxo
-
-        // get ScriptContext from the transaction
-        val context = Interop.getScriptContextV3(
-          redeemer = tx.getWitnessSet.getRedeemers.get(0),
-          datum = Some(datum),
-          tx = tx,
-          txhash = TransactionUtil.getTxHash(tx.serialize()),
-          utxos = utxos,
-          slotConfig = SlotConfig.Mainnet,
-          protocolVersion = 10
-        )
-        (utxos, context)
+    test("receiver can't unlock HTLC with invalid preimage") {
+        val validPreimage = generatePreimage()
+        val invalidPreimage = generateInvalidPreimage()
+        TestCase(
+          inputs = List(Receiver gives 100),
+          outputs = List(Receiver gets 1050),
+          value = 1000,
+          fee = 50,
+          timeout = 1000,
+          validFrom = 500,
+          signatories = List(Receiver),
+          action = Action.Reveal(invalidPreimage),
+          preimage = validPreimage,
+          expected = failure(InvalidReceiverPreimage)
+        ).runWithDebug()
     }
 
-    def makeUnlockingTransaction(
-        input: TransactionInput,
-        scriptRefInput: TransactionInput,
-        startTimeMillis: PosixTime,
-        action: Action,
-        signatories: Seq[PubKeyHash]
-    ): Transaction = {
-        import Interop.*
-
-        import scala.jdk.CollectionConverters.*
-        val redeemer = action.toData
-
-        val rdmr = Redeemer
-            .builder()
-            .tag(RedeemerTag.Spend)
-            .data(toPlutusData(redeemer))
-            .index(0)
-            .exUnits(
-              ExUnits
-                  .builder()
-                  .steps(BigInteger.valueOf(1000))
-                  .mem(BigInteger.valueOf(1000))
-                  .build()
-            )
-            .build()
-
-        val inputs = util.List.of(input)
-
-        val tx = Transaction
-            .builder()
-            .body(
-              TransactionBody
-                  .builder()
-                  .validityStartInterval(
-                    SlotConfig.Mainnet.timeToSlot(startTimeMillis.toLong)
-                  )
-                  .fee(ADAConversionUtil.adaToLovelace(0.2))
-                  .inputs(inputs)
-                  .referenceInputs(util.List.of(scriptRefInput))
-                  .requiredSigners(signatories.map(_.hash.bytes).asJava)
-                  .build()
-            )
-            .witnessSet(TransactionWitnessSet.builder().redeemers(util.List.of(rdmr)).build())
-            .build()
-        tx
+    test("committer fails to unlock HTLC without signature") {
+        TestCase(
+          inputs = List(Committer gives 100),
+          outputs = List(Committer gets 1050),
+          value = 1000,
+          fee = 50,
+          timeout = 500,
+          validFrom = 1000,
+          signatories = List.empty,
+          action = Action.Timeout,
+          preimage = generatePreimage(),
+          expected = failure(UnsignedCommitterTransaction)
+        ).runWithDebug()
     }
+
+    test("receiver fails to unlock HTLC without signature") {
+        val preimage = generatePreimage()
+        TestCase(
+          inputs = List(Receiver gives 100),
+          outputs = List(Receiver gets 1050),
+          value = 1000,
+          fee = 50,
+          timeout = 1000,
+          validFrom = 500,
+          signatories = List.empty,
+          action = Action.Reveal(preimage),
+          preimage = preimage,
+          expected = failure(UnsignedReceiverTransaction)
+        ).runWithDebug()
+    }
+}
+
+object HtlcValidatorTest extends ScalusTest {
+    export HtlcValidator.*
+    export Person.*
 
     enum Person(val pkh: Hash):
         case Committer extends Person(genByteStringOfN(28).sample.get)
         case Receiver extends Person(genByteStringOfN(28).sample.get)
 
-    extension (self: Person)
-        infix def gives(value: BigInt): Input = Input(self, value)
-        infix def gets(value: BigInt): Output = Output(self, value)
+        infix def gives(value: BigInt): Input = Input(this, value)
+        infix def gets(value: BigInt): Output = Output(this, value)
 
     case class Input(person: Person, value: BigInt)
     case class Output(person: Person, value: BigInt)
@@ -271,6 +183,9 @@ class HtlcValidatorTest extends AnyFunSuite with ScalusTest {
             checkResult(expected = expected, actual = script.runWithDebug(context))
         }
 
+    private def generatePreimage(): Preimage = genByteStringOfN(32).sample.get
+    private def generateInvalidPreimage(): Preimage = genByteStringOfN(12).sample.get
+
     private def makeSpendingScriptContext(
         inputs: List[TxInInfo] = List.empty,
         outputs: List[TxOut] = List.empty,
@@ -298,11 +213,5 @@ class HtlcValidatorTest extends AnyFunSuite with ScalusTest {
         )
     }
 
-    private lazy val sender = new Account()
-    private lazy val script = {
-        HtlcValidator.script
-    }
     private lazy val scriptHash = script.hash
-    private val costMdls = CostMdls()
-    costMdls.add(CostModelUtil.PlutusV3CostModel)
 }
