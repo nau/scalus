30d7684c2214acd4be9ddf55a833f2dc42e3edc5
Alexander Nemish <anemish@gmail.com>
2025-10-18 15:00:38 +0200

    refactor: update builtin cost models to use CardanoInfo
    Reduces scalus-cardano-ledger-opt-bundle.js by ~100k

--- scalus-core/shared/src/main/scala/scalus/uplc/Meaning.scala
+++ scalus-core/shared/src/main/scala/scalus/uplc/Meaning.scala
@@ -1,10 +1,12 @@
 package scalus.uplc
 
 import scalus.builtin.{platform, PlatformSpecific}
+import scalus.cardano.ledger.{CardanoInfo, Language}
+import scalus.uplc.eval.MachineParams.fromProtocolParams
 
 object Meaning {
     lazy val allBuiltins: CardanoBuiltins = CardanoBuiltins(
-      eval.BuiltinCostModel.defaultCostModelC,
+      fromProtocolParams(CardanoInfo.mainnet.protocolParams, Language.PlutusV3).builtinCostModel,
       platform,
       BuiltinSemanticsVariant.C
     )
