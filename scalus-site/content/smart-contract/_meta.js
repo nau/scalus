cc2efd4ef86a3e75d1d0b0725ceb943c28750488
otto <sae3023@gmail.com>
2025-10-21 09:24:55 +0200

    doc: `from-data`, touch up in-depth validator

--- scalus-site/content/smart-contract/_meta.js
+++ scalus-site/content/smart-contract/_meta.js
@@ -1,7 +1,8 @@
 export default {
-    //index: '',
+    index: '',
     // 'building-plutus-script': 'Building plutus script',
     // 'evaluating-script': 'Evaluating script',
+    'in-depth-validator-look': 'In Depth Validator Walkthrough',
     'from-data': 'Extracting from Data',
     'testing': 'Testing Validator',
     'debugging': 'Troubleshooting',
