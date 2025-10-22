f3590ba304f6c03d5618a31e9597a93e6e74681a
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2025-10-22 02:01:28 +0200

    testing: Fibbonaci

--- scalus-examples/jvm/src/test/scala/scalus/examples/FibonacciTest.scala
+++ scalus-examples/jvm/src/test/scala/scalus/examples/FibonacciTest.scala
@@ -85,6 +85,7 @@ class FibonacciTest extends AnyFunSuite with ScalusTest {
         println(sir.showHighlighted)
         val uplc: Term = sir.toUplc() |> Inliner.apply
         println(s"\n${uplc.pretty.render(Int.MaxValue)}\n")
+        println(uplc.plutusV3.cborByteString.size)
         val x = uplc.evaluate
         println(x)
         assert(s"$x" === "Const(Integer(354224848179261915075))")
